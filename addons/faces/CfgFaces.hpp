0f3292c4dbfe36eb9abf34e9fd90c3ccfd389bf1
Tupolov <tupolov73@gmail.com>
2017-10-30 10:13:58 +0000

    [faces] Merge skins with faces

---
+++ addons/faces/CfgFaces.hpp
@@ -0,0 +1,168 @@
+class CfgFaces
+{
+    class Default
+    {
+        class Custom;
+    };
+    class Man_A3: Default
+    {
+        class Default
+        {
+            displayname = "$STR_CFG_FACES_Default";
+            texture = "\A3\Characters_F\Heads\Data\m_White_01_co.paa";
+            head = "DefaultHead_A3";
+            identityTypes[] = {};
+            material = "A3\Characters_F\Heads\Data\m_White_01.rvmat";
+            textureHL = "\A3\Characters_F\Heads\Data\hl_White_hairy_1_co.paa";
+            materialHL = "\A3\Characters_F\Heads\Data\hl_White_hairy_muscular.rvmat";
+            textureHL2 = "\A3\Characters_F\Heads\Data\hl_White_hairy_1_co.paa";
+            materialHL2 = "\A3\Characters_F\Heads\Data\hl_White_hairy_muscular.rvmat";
+            disabled = 0;
+        };
+        class SP_WoodlandFace: Default
+        {
+            displayname = "SP Woodland Face 1";
+            texture = "\x\cfp\addons\faces\data\Woodland1.paa";
+            head = "DefaultHead_A3";
+            identityTypes[] = {};
+            material = "\x\cfp\addons\faces\data\custom.rvmat";
+            textureHL = "\A3\Characters_F\Heads\Data\hl_White_hairy_1_co.paa";
+            materialHL = "\A3\Characters_F\Heads\Data\hl_White_hairy_muscular.rvmat";
+            textureHL2 = "\A3\Characters_F\Heads\Data\hl_White_hairy_1_co.paa";
+            materialHL2 = "\A3\Characters_F\Heads\Data\hl_White_hairy_muscular.rvmat";
+            disabled = 0;
+        };
+        class SP_WoodlandFace1: SP_WoodlandFace
+        {
+            displayname = "SP Woodland Face 1";
+            texture = "\x\cfp\addons\faces\data\Woodland1.paa";
+        };
+        class SP_WoodlandFace2: SP_WoodlandFace
+        {
+            displayname = "SP Woodland Face 2";
+            texture = "\x\cfp\addons\faces\data\Woodland2.paa";
+        };
+        class SP_WoodlandFace3: SP_WoodlandFace
+        {
+            displayname = "SP Woodland Face 3";
+            texture = "\x\cfp\addons\faces\data\Woodland3.paa";
+        };
+        class Tattoo1: Default
+        {
+            name = "Tattoo1";
+            displayname = "Tattoo 1";
+            texture = "\A3\Characters_F\Heads\Data\m_White_01_co.paa";
+            head = "DefaultHead_A3";
+            identityTypes[] = {};
+            material = "A3\Characters_F\Heads\Data\m_white_01.rvmat";
+            textureHL = "\x\cfp\addons\faces\data\Tattoo1.jpg";
+            materialHL = "\A3\Characters_F\Heads\Data\hl_white.rvmat";
+            textureHL2 = "\x\cfp\addons\faces\data\Tattoo1.jpg";
+            materialHL2 = "\A3\Characters_F\Heads\Data\hl_white.rvmat";
+        };
+        class Tattoo2: Default
+        {
+            name = "Tattoo2";
+            displayname = "Tattoo 2";
+            texture = "\A3\Characters_F\Heads\Data\m_White_01_co.paa";
+            head = "DefaultHead_A3";
+            identityTypes[] = {};
+            material = "A3\Characters_F\Heads\Data\m_white_01.rvmat";
+            textureHL = "\x\cfp\addons\faces\data\Tattoo2.jpg";
+            materialHL = "\A3\Characters_F\Heads\Data\hl_white.rvmat";
+            textureHL2 = "\x\cfp\addons\faces\data\Tattoo2.jpg";
+            materialHL2 = "\A3\Characters_F\Heads\Data\hl_white.rvmat";
+        };
+        class Tattoo3: Default
+        {
+            name = "Tattoo3";
+            displayname = "Tattoo 3";
+            texture = "\A3\Characters_F\Heads\Data\m_White_01_co.paa";
+            head = "DefaultHead_A3";
+            identityTypes[] = {};
+            material = "A3\Characters_F\Heads\Data\m_white_01.rvmat";
+            textureHL = "\x\cfp\addons\faces\data\Tattoo3.jpg";
+            materialHL = "\A3\Characters_F\Heads\Data\hl_white.rvmat";
+            textureHL2 = "\x\cfp\addons\faces\data\Tattoo3.jpg";
+            materialHL2 = "\A3\Characters_F\Heads\Data\hl_white.rvmat";
+        };
+        class Tattoo4: Default
+        {
+            name = "Tattoo4";
+            displayname = "Tattoo 4";
+            texture = "\A3\Characters_F\Heads\Data\m_White_01_co.paa";
+            head = "DefaultHead_A3";
+            identityTypes[] = {};
+            material = "A3\Characters_F\Heads\Data\m_white_01.rvmat";
+            textureHL = "\x\cfp\addons\faces\data\Tattoo4.jpg";
+            materialHL = "\A3\Characters_F\Heads\Data\hl_white.rvmat";
+            textureHL2 = "\x\cfp\addons\faces\data\Tattoo4.jpg";
+            materialHL2 = "\A3\Characters_F\Heads\Data\hl_white.rvmat";
+        };
+        class Tattoo5: Default
+        {
+            name = "Tattoo5";
+            displayname = "Tattoo 5";
+            texture = "\A3\Characters_F\Heads\Data\m_White_01_co.paa";
+            head = "DefaultHead_A3";
+            identityTypes[] = {};
+            material = "A3\Characters_F\Heads\Data\m_white_01.rvmat";
+            textureHL = "\x\cfp\addons\faces\data\Tattoo5.jpg";
+            materialHL = "\A3\Characters_F\Heads\Data\hl_white.rvmat";
+            textureHL2 = "\x\cfp\addons\faces\data\Tattoo5.jpg";
+            materialHL2 = "\A3\Characters_F\Heads\Data\hl_white.rvmat";
+        };
+        class Tattoo6: Default
+        {
+            name = "Tattoo6";
+            displayname = "Tattoo 6";
+            texture = "\A3\Characters_F\Heads\Data\m_White_01_co.paa";
+            head = "DefaultHead_A3";
+            identityTypes[] = {};
+            material = "A3\Characters_F\Heads\Data\m_white_01.rvmat";
+            textureHL = "\x\cfp\addons\faces\data\Tattoo6.jpg";
+            materialHL = "\A3\Characters_F\Heads\Data\hl_white.rvmat";
+            textureHL2 = "\x\cfp\addons\faces\data\Tattoo6.jpg";
+            materialHL2 = "\A3\Characters_F\Heads\Data\hl_white.rvmat";
+        };
+        class Tattoo7: Default
+        {
+            name = "Tattoo7";
+            displayname = "Tattoo 7";
+            texture = "\A3\Characters_F\Heads\Data\m_White_01_co.paa";
+            head = "DefaultHead_A3";
+            identityTypes[] = {};
+            material = "A3\Characters_F\Heads\Data\m_white_01.rvmat";
+            textureHL = "\x\cfp\addons\faces\data\Tattoo7.jpg";
+            materialHL = "\A3\Characters_F\Heads\Data\hl_white.rvmat";
+            textureHL2 = "\x\cfp\addons\faces\data\Tattoo7.jpg";
+            materialHL2 = "\A3\Characters_F\Heads\Data\hl_white.rvmat";
+        };
+        class Tattoo8: Default
+        {
+            name = "Tattoo8";
+            displayname = "Tattoo 8";
+            texture = "\A3\Characters_F\Heads\Data\m_White_01_co.paa";
+            head = "DefaultHead_A3";
+            identityTypes[] = {};
+            material = "A3\Characters_F\Heads\Data\m_white_01.rvmat";
+            textureHL = "\x\cfp\addons\faces\data\Tattoo8.jpg";
+            materialHL = "\A3\Characters_F\Heads\Data\hl_white.rvmat";
+            textureHL2 = "\x\cfp\addons\faces\data\Tattoo8.jpg";
+            materialHL2 = "\A3\Characters_F\Heads\Data\hl_white.rvmat";
+        };
+        class Tattoo9: Default
+        {
+            name = "Tattoo9";
+            displayname = "Tattoo 9";
+            texture = "\A3\Characters_F\Heads\Data\m_White_01_co.paa";
+            head = "DefaultHead_A3";
+            identityTypes[] = {};
+            material = "A3\Characters_F\Heads\Data\m_white_01.rvmat";
+            textureHL = "\x\cfp\addons\faces\data\Tattoo9.jpg";
+            materialHL = "\A3\Characters_F\Heads\Data\hl_white.rvmat";
+            textureHL2 = "\x\cfp\addons\faces\data\Tattoo9.jpg";
+            materialHL2 = "\A3\Characters_F\Heads\Data\hl_white.rvmat";
+        };
+    };
+};
