464456855d9ed9b822e1b35ecaead6f308db0354
AUTigerGrad <audrew77@yahoo.com>
2017-11-04 10:17:39 -0400

    Deleted duplicate entries in configs
    Deleted all duplicate entires in Taliban configs

--- addons/o_tban/script_component.hpp
+++ addons/o_tban/script_component.hpp
@@ -11,29 +11,3 @@
 #define DEBUG_SETTINGS DEBUG_SETTINGS_O_TBAN
 #endif
 #include "\x\cfp\addons\main\script_macros.hpp"
-#define COMPONENT o_tban
-#define COMPONENT_BEAUTIFIED o_tban
-#include "\x\cfp\addons\main\script_mod.hpp"
-// #define DEBUG_MODE_FULL
-// #define DISABLE_COMPILE_CACHE
-// #define ENABLE_PERFORMANCE_COUNTERS
-#ifdef DEBUG_ENABLED_O_TBAN
-#define DEBUG_MODE_FULL
-#endif
-#ifdef DEBUG_SETTINGS_O_TBAN
-#define DEBUG_SETTINGS DEBUG_SETTINGS_O_TBAN
-#endif
-#include "\x\cfp\addons\main\script_macros.hpp"
-#define COMPONENT o_tban
-#define COMPONENT_BEAUTIFIED o_tban
-#include "\x\cfp\addons\main\script_mod.hpp"
-// #define DEBUG_MODE_FULL
-// #define DISABLE_COMPILE_CACHE
-// #define ENABLE_PERFORMANCE_COUNTERS
-#ifdef DEBUG_ENABLED_O_TBAN
-#define DEBUG_MODE_FULL
-#endif
-#ifdef DEBUG_SETTINGS_O_TBAN
-#define DEBUG_SETTINGS DEBUG_SETTINGS_O_TBAN
-#endif
-#include "\x\cfp\addons\main\script_macros.hpp"
