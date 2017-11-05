e8d295ce65d28261fa393a20516591d4fedd8ad4
AUTigerGrad <audrew77@yahoo.com>
2017-11-05 13:43:57 -0500

    Added Afghan Police Faction

---
+++ addons/b_afgpolice/CfgPatches.hpp
@@ -0,0 +1,21 @@
+class CfgPatches {
+    class CFP_B_AFGPolice {
+        units[] = {
+            "CFP_B_AFGPolice_Rifleman_AK74_01",
+            "CFP_B_AFGPolice_Grenadier_AK74_01",
+            "CFP_B_AFGPolice_Rifleman_AK74M_01",
+            "CFP_B_AFGPolice_Officer_01",
+            "CFP_B_AFGPolice_Grenadier_AK74M_01",
+            "CFP_B_AFGPolice_Machine_Gunner_PKM_01",
+            "CFP_B_AFGPolice_Rifleman_AT_RPG_01"
+        };
+        weapons[] = {};
+        requiredVersion = 1.62;
+        requiredAddons[] = {
+            "A3_Characters_F",
+            "CFP_Uniforms"
+        };
+        author = "Drew";
+        authors[] = { "Drew" };
+    };
+};
