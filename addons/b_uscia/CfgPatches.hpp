7b9d2c829e9e1e5a3fc209ba2444d25535f673f4
Tupolov <mcoppinger@lulz.home>
2017-11-11 20:26:27 +0000

    [b_uscia] Added WIP US CIA faction

---
+++ addons/b_uscia/CfgPatches.hpp
@@ -0,0 +1,22 @@
+class CfgPatches {
+    class CFP_B_USCIA {
+        units[] = {
+        	"CFP_B_USCIA_Paramilitary_Officer_Base",
+			"CFP_B_USCIA_paramilitary_officer_01",
+			"CFP_B_USCIA_paramilitary_officer_covert_01",
+			"CFP_B_USCIA_paramilitary_officer_assault_01",
+			"CFP_B_USCIA_paramilitary_officer_assault_02",
+			"CFP_B_USCIA_paramilitary_officer_assault_03",
+			"CFP_B_USCIA_paramilitary_officer_assault_04"
+        };
+        weapons[] = {};
+        requiredAddons[] = {
+            "CFP_Main",
+            "CFP_Uniforms"
+        };
+      	author = MODULE_AUTHOR;
+      	authors[] = {"Tupolov"};
+      	requiredVersion = REQUIRED_VERSION;
+      	VERSION_CONFIG;
+    };
+};
