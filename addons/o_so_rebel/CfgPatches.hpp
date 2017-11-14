5960f561d97fcb21a9ea6922a805838befd572ca
AUTigerGrad <audrew77@yahoo.com>
2017-11-14 13:11:54 -0500

    Added Somali Rebels Faction

---
+++ addons/o_so_rebel/CfgPatches.hpp
@@ -0,0 +1,48 @@
+class CfgPatches {
+    class CFP_O_SO_REBEL {
+        units[] = {
+            "CFP_O_SO_REBEL_Rifleman_01",
+            "CFP_O_SO_REBEL_Sharpshooter_01",
+            "CFP_O_SO_REBEL_Grenadier_01",
+            "CFP_O_SO_REBEL_Rifleman_Light_01",
+            "CFP_O_SO_REBEL_Rifleman_2_01",
+            "CFP_O_SO_REBEL_Warlord_01",
+            "CFP_O_SO_REBEL_Autorifleman_01",
+            "CFP_O_SO_REBEL_Autorifleman_2_01",
+            "CFP_O_SO_REBEL_Bonesetter_01",
+            "CFP_O_SO_REBEL_Rifleman_AT_01",
+            "CFP_O_SO_REBEL_Marksman_01",
+            "CFP_O_SO_REBEL_Rifleman_AA_01",
+            "CFP_O_SO_REBEL_Rifleman_AT_2_01",
+            "CFP_O_SO_REBEL_Warlord_2_01",
+            "CFP_O_SO_REBEL_Grenadier_2_01",
+            "CFP_O_SO_REBEL_Rifleman_3_01",
+            "CFP_O_SO_REBEL_Bonesetter_2_01",
+            "CFP_O_SO_REBEL_Rifleman_AA_2_01",
+            "CFP_O_SO_REBEL_Technical_PK_01",
+            "CFP_O_SO_REBEL_Truck_01",
+            "CFP_O_SO_REBEL_Technical_DSHkM_01",
+            "CFP_O_SO_REBEL_Technical_SPG_01",
+            "CFP_O_SO_REBEL_DShKM_01",
+            "CFP_O_SO_REBEL_Gunner_DShKM_High_01",
+            "CFP_O_SO_REBEL_Gunner_DShKM_Low_01",
+            "CFP_O_SO_REBEL_Gunner_SPG_01",
+            "CFP_O_SO_REBEL_Gunner_Mortar_01",
+            "CFP_O_SO_REBEL_Bombmaker_01",
+            "CFP_O_SO_REBEL_Bombmaker_2_01"
+        };
+        weapons[] = {};
+        requiredVersion = 1.62;
+        requiredAddons[] = {
+            "A3_Characters_F",
+            "CUP_Creatures_Military_SLA",
+            "CUP_Creatures_Military_Taki",
+            "CUP_WheeledVehicles_Datsun",
+            "CUP_WheeledVehicles_V3S",
+            "CUP_WheeledVehicles_UAZ",
+            "CUP_Weapons_StaticWeapons"
+        };
+        author = "Drew";
+        authors[] = { "Drew" };
+    };
+};
---
+++ addons/o_so_rebel/script_component.hpp
@@ -0,0 +1,13 @@
+#define COMPONENT o_so_rebel
+#define COMPONENT_BEAUTIFIED o_so_rebel
+#include "\x\cfp\addons\main\script_mod.hpp"
+// #define DEBUG_MODE_FULL
+// #define DISABLE_COMPILE_CACHE
+// #define ENABLE_PERFORMANCE_COUNTERS
+#ifdef DEBUG_ENABLED_O_SO_REBEL
+#define DEBUG_MODE_FULL
+#endif
+#ifdef DEBUG_SETTINGS_O_SO_REBEL
+#define DEBUG_SETTINGS DEBUG_SETTINGS_O_SO_REBEL
+#endif
+#include "\x\cfp\addons\main\script_macros.hpp"
