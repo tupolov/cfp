cab396afc8c2c4b14e7032d07fe5e25f7bc186b7
AUTigerGrad <audrew77@yahoo.com>
2017-11-14 14:17:47 -0500

    Added Middle Eastern Civilians

---
+++ addons/c_me/CfgPatches.hpp
@@ -0,0 +1,48 @@
+class CfgPatches {
+    class CFP_C_ME {
+        units[] = {
+            "CFP_C_ME_Civ_1_01",
+            "CFP_C_ME_Civ_2_01",
+            "CFP_C_ME_Civ_3_01",
+            "CFP_C_ME_Civ_4_01",
+            "CFP_C_ME_Civ_5_01",
+            "CFP_C_ME_Civ_6_01",
+            "CFP_C_ME_Civ_7_01",
+            "CFP_C_ME_Civ_8_01",
+            "CFP_C_ME_Civ_9_01",
+            "CFP_C_ME_Civ_10_01",
+            "CFP_C_ME_Civ_11_01",
+            "CFP_C_ME_Civ_12_01",
+            "CFP_C_ME_Civ_13_01",
+            "CFP_C_ME_Civ_14_01",
+            "CFP_C_ME_Ikarus_01",
+            "CFP_C_ME_Land_Rover_01",
+            "CFP_C_ME_UAZ_01",
+            "CFP_C_ME_SUV_01",
+            "CFP_C_ME_Skoda_01",
+            "CFP_C_ME_Datsun_Pickup_01",
+            "CFP_C_ME_Datsun_Pickup_Covered_01",
+            "CFP_C_ME_Golf_Red_01",
+            "CFP_C_ME_Skoda_Octavia_01",
+            "CFP_C_ME_Truck_01"
+        };
+        weapons[] = {};
+        requiredVersion = 1.62;
+        requiredAddons[] = {
+            "A3_Characters_F",
+            "CUP_WheeledVehicles_Ikarus",
+            "CUP_WheeledVehicles_LR",
+            "CUP_WheeledVehicles_UAZ",
+            "CUP_Wheeled_SUV",
+            "CUP_WheeledVehicles_Skoda",
+            "CUP_WheeledVehicles_Datsun",
+            "CUP_Wheeledvehicles_VWGolf",
+            "CUP_WheeledVehicles_T810",
+            "A3_Soft_F",
+            "A3_Soft_F_Exp_Offroad_02",
+            "A3_Soft_F_Gamma_Van_01"
+        };
+        author = "Drew";
+        authors[] = { "Drew" };
+    };
+};
---
+++ addons/c_me/script_component.hpp
@@ -0,0 +1,13 @@
+#define COMPONENT c_me
+#define COMPONENT_BEAUTIFIED c_me
+#include "\x\cfp\addons\main\script_mod.hpp"
+// #define DEBUG_MODE_FULL
+// #define DISABLE_COMPILE_CACHE
+// #define ENABLE_PERFORMANCE_COUNTERS
+#ifdef DEBUG_ENABLED_C_ME
+#define DEBUG_MODE_FULL
+#endif
+#ifdef DEBUG_SETTINGS_C_ME
+#define DEBUG_SETTINGS DEBUG_SETTINGS_C_ME
+#endif
+#include "\x\cfp\addons\main\script_macros.hpp"
