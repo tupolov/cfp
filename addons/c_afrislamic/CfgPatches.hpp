20117948206c1ba51ab4cf0d1d38d392dea7d2fa
AUTigerGrad <audrew77@yahoo.com>
2017-11-14 14:59:54 -0500

    Added Islamic African Civilian Faction

---
+++ addons/c_afrislamic/CfgPatches.hpp
@@ -0,0 +1,41 @@
+class CfgPatches {
+    class CFP_C_AFRISLAMIC {
+        units[] = {
+            "CFP_C_AFRISLAMIC_Civ_1_01",
+            "CFP_C_AFRISLAMIC_Civ_2_01",
+            "CFP_C_AFRISLAMIC_Civ_3_01",
+            "CFP_C_AFRISLAMIC_Civ_4_01",
+            "CFP_C_AFRISLAMIC_Civ_5_01",
+            "CFP_C_AFRISLAMIC_Civ_6_01",
+            "CFP_C_AFRISLAMIC_Civ_7_01",
+            "CFP_C_AFRISLAMIC_Civ_8_01",
+            "CFP_C_AFRISLAMIC_Civ_9_01",
+            "CFP_C_AFRISLAMIC_Civ_10_01",
+            "CFP_C_AFRISLAMIC_Civ_11_01",
+            "CFP_C_AFRISLAMIC_Civ_12_01",
+            "CFP_C_AFRISLAMIC_Zamak_01",
+            "CFP_C_AFRISLAMIC_Truck_Boxer_01",
+            "CFP_C_AFRISLAMIC_Skoda_White_01",
+            "CFP_C_AFRISLAMIC_Skoda_Blue_01",
+            "CFP_C_AFRISLAMIC_Skoda_Green_01",
+            "CFP_C_AFRISLAMIC_Datsun_Pickup_Old_01",
+            "CFP_C_AFRISLAMIC_Ural_Yellow_01",
+            "CFP_C_AFRISLAMIC_Ural_Blue_01",
+            "CFP_C_AFRISLAMIC_Land_Rover_01"
+        };
+        weapons[] = {};
+        requiredVersion = 1.62;
+        requiredAddons[] = {
+            "A3_Characters_F",
+            "A3_Soft_F_Beta_Truck_02",
+            "A3_Soft_F_Gamma_Truck_02",
+            "A3_Soft_F_Gamma_Van_01",
+            "CUP_WheeledVehicles_Skoda",
+            "CUP_WheeledVehicles_Datsun",
+            "CUP_WheeledVehicles_Ural",
+            "CUP_WheeledVehicles_LR"
+        };
+        author = "Drew";
+        authors[] = { "Drew" };
+    };
+};
