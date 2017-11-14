af429993f7f0a89c021fc6cd8d9663518a0283f1
AUTigerGrad <audrew77@yahoo.com>
2017-11-14 15:49:46 -0500

    Added Asian Civilian Faction

---
+++ addons/c_asia/CfgPatches.hpp
@@ -0,0 +1,49 @@
+class CfgPatches {
+    class CFP_C_ASIA {
+        units[] = {
+            "CFP_C_ASIA_Civilian_1_01",
+            "CFP_C_ASIA_Civilian_2_01",
+            "CFP_C_ASIA_Civilian_3_01",
+            "CFP_C_ASIA_Civilian_4_01",
+            "CFP_C_ASIA_Civilian_5_01",
+            "CFP_C_ASIA_Civilian_6_01",
+            "CFP_C_ASIA_Civilian_7_01",
+            "CFP_C_ASIA_Civilian_8_01",
+            "CFP_C_ASIA_Civilian_9_01",
+            "CFP_C_ASIA_Civilian_10_01",
+            "CFP_C_ASIA_Civilian_11_01",
+            "CFP_C_ASIA_Civilian_12_01",
+            "CFP_C_ASIA_Land_Rover_01",
+            "CFP_C_ASIA_UAZ_01",
+            "CFP_C_ASIA_Praga_V3S_01",
+            "CFP_C_ASIA_SUV_01",
+            "CFP_C_ASIA_Skoda_Octavia_01",
+            "CFP_C_ASIA_Water_Scooter_01",
+            "CFP_C_ASIA_Fishing_Boat_01",
+            "CFP_C_ASIA_Datsun_Pickup_01",
+            "CFP_C_ASIA_Ural_01",
+            "CFP_C_ASIA_MB_4WD_01",
+            "CFP_C_ASIA_Offroad_01"
+        };
+        weapons[] = {};
+        requiredVersion = 1.62;
+        requiredAddons[] = {
+            "A3_Characters_F_Exp",
+            "A3_Characters_F",
+            "CUP_WheeledVehicles_LR",
+            "CUP_WheeledVehicles_UAZ",
+            "CUP_WheeledVehicles_V3S",
+            "CUP_Wheeled_SUV",
+            "A3_Soft_F",
+            "A3_Boat_F_Exp_Scooter_Transport_01",
+            "CUP_WaterVehicles_Fishing_Boat",
+            "CUP_WheeledVehicles_Datsun",
+            "CUP_WheeledVehicles_Ural",
+            "A3_Soft_F_Exp_Offroad_02",
+            "A3_Soft_F_Offroad_01",
+            "A3_Soft_F_Exp_Offroad_01"
+        };
+        author = "Drew";
+        authors[] = { "Drew" };
+    };
+};
---
+++ addons/c_asia/script_component.hpp
@@ -0,0 +1,13 @@
+#define COMPONENT c_asia
+#define COMPONENT_BEAUTIFIED c_asia
+#include "\x\cfp\addons\main\script_mod.hpp"
+// #define DEBUG_MODE_FULL
+// #define DISABLE_COMPILE_CACHE
+// #define ENABLE_PERFORMANCE_COUNTERS
+#ifdef DEBUG_ENABLED_C_ASIA
+#define DEBUG_MODE_FULL
+#endif
+#ifdef DEBUG_SETTINGS_C_ASIA
+#define DEBUG_SETTINGS DEBUG_SETTINGS_C_ASIA
+#endif
+#include "\x\cfp\addons\main\script_macros.hpp"
