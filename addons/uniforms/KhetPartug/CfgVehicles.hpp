25f233cc7d18867e5558dea487a9164d40ece595
Tupolov <tupolov73@gmail.com>
2017-11-18 10:23:22 +0000

    [uniforms] Added new Khet Partug camo and standard olive uniforms

--- addons/uniforms/KhetPartug/CfgVehicles.hpp
+++ addons/uniforms/KhetPartug/CfgVehicles.hpp
@@ -60,33 +60,38 @@
 		};
 	};
 
-	class CFP_KhetPartug_Long_olive: B_Soldier_base_F
+	class CFP_KhetPartug_Long_olive: CFP_KhetPartug_Long_M81
 	{
-
-		scope = 1;
 		displayName = "Khet Partug, Long (Olive)";
-		faction = "BLU_F";
-		modelSides[] = {6};
-		vehicleClass = "CFP_KhetPartug";
-		canCarryBackPack=1;
-		canDeactivateMines=1;
-		engineer=1;
-		attendant = "true";
-		uniformAccessories[] = {};
-		uniformClass = "CFP_U_KhetPartug_Long_olive";
-		nakedUniform = "U_BasicBody";
-		model = "\CUP\Creatures\People\Military\CUP_Creatures_People_Military_TakiInsurgents\CUP_meunit_1.p3d";
-		editorPreview = CUP\Creatures\People\Civil\CUP_Creatures_People_Civil_Takistan\Data\preview\CUP_C_TK_MAN_02.jpg;
-		Items[] = {};
-		weapons[] = {"Throw","Put"};
-		magazines[] = {};
-		linkedItems[] = {"ItemWatch","ItemRadio","ItemGPS"};
-		respawnLinkedItems[] = {"ItemMap","ItemCompass","ItemWatch","ItemRadio","ItemGPS"};
-		hiddenSelectionsTextures[] = {"\x\cfp\addons\uniforms\KhetPartug\camo\KhetPartug_Long_olive.paa"};
-		hiddenSelections[] = {"Camo", "insignia"};
-		class Wounds /// changes material of skin according to damage
-		{
-			tex[] = {};
-			mat[] = {"A3\Characters_F_Beta\INDEP\Data\ia_soldier_01_clothing.rvmat","A3\Characters_F_Beta\INDEP\Data\ia_soldier_01_clothing_injury.rvmat","A3\Characters_F_Beta\INDEP\Data\ia_soldier_01_clothing_injury.rvmat","A3\Characters_F\Heads\Data\hl_white_bald_muscular.rvmat","A3\Characters_F\Heads\Data\hl_white_bald_muscular_injury.rvmat","A3\Characters_F\Heads\Data\hl_white_bald_muscular_injury.rvmat","A3\Characters_F\Heads\Data\hl_black_bald_muscular.rvmat","A3\Characters_F\Heads\Data\hl_black_bald_muscular_injury.rvmat","A3\Characters_F\Heads\Data\hl_black_bald_muscular_injury.rvmat","A3\Characters_F\Heads\Data\hl_white_hairy_muscular.rvmat","A3\Characters_F\Heads\Data\hl_white_hairy_muscular_injury.rvmat","A3\Characters_F\Heads\Data\hl_white_hairy_muscular_injury.rvmat","A3\Characters_F\Heads\Data\hl_white_old.rvmat","A3\Characters_F\Heads\Data\hl_white_old_injury.rvmat","A3\Characters_F\Heads\Data\hl_white_old_injury.rvmat","A3\Characters_F\Heads\Data\hl_asian_bald_muscular.rvmat","A3\Characters_F\Heads\Data\hl_asian_bald_muscular_injury.rvmat","A3\Characters_F\Heads\Data\hl_asian_bald_muscular_injury.rvmat"};
-		};
+		hiddenSelectionsTextures[] = {"\x\cfp\addons\uniforms\KhetPartug\standard\KhetPartug_Long_olive.paa"};
+	};
+
+	class CFP_KhetPartug_short_olive: CFP_KhetPartug_Short_M81
+	{
+		displayName = "Khet Partug, Short (Olive)";
+		hiddenSelectionsTextures[] = {"\x\cfp\addons\uniforms\KhetPartug\standard\KhetPartug_short_olive.paa"};
+	};
+
+	class CFP_KhetPartug_Long_light_olive: CFP_KhetPartug_Long_M81
+	{
+		displayName = "Khet Partug, Long (Light Olive)";
+		hiddenSelectionsTextures[] = {"\x\cfp\addons\uniforms\KhetPartug\standard\KhetPartug_Long_light_olive.paa"};
+	};
+
+	class CFP_KhetPartug_short_light_olive: CFP_KhetPartug_Short_M81
+	{
+		displayName = "Khet Partug, Short (Light Olive)";
+		hiddenSelectionsTextures[] = {"\x\cfp\addons\uniforms\KhetPartug\standard\KhetPartug_short_light_olive.paa"};
+	};
+
+	class CFP_KhetPartug_Long_olive_polygon: CFP_KhetPartug_Long_M81
+	{
+		displayName = "Khet Partug, Long (Olive / Desert Polygon)";
+		hiddenSelectionsTextures[] = {"\x\cfp\addons\uniforms\KhetPartug\camo\KhetPartug_Long_olive_polygon.paa"};
+	};
+
+	class CFP_KhetPartug_short_olive_polygon: CFP_KhetPartug_Short_M81
+	{
+		displayName = "Khet Partug, Short (Olive / Desert Polygon)";
+		hiddenSelectionsTextures[] = {"\x\cfp\addons\uniforms\KhetPartug\camo\KhetPartug_short_olive_polygon.paa"};
 	};
--- addons/uniforms/KhetPartug/CfgWeapons.hpp
+++ addons/uniforms/KhetPartug/CfgWeapons.hpp
@@ -32,18 +32,79 @@
 		};
 	};
 
-	class CFP_U_KhetPartug_Long_olive: Uniform_Base
+	class CFP_U_KhetPartug_Long_olive: CFP_U_KhetPartug_Long_M81
 	{
-		scope=2;
-		model = "\x\cfp\addons\models\Suitpack";
 		displayName = "Khet Partug (Olive)";
-		picture = "\CUP\Creatures\People\Military\CUP_Creatures_People_Military_TakiInsurgents\data\ui\icon_u_o_khet_02_ca.paa";
-		author = "AccuracyThruVolume";
 
 		class ItemInfo: UniformItem
 		{
 			uniformModel = "-";
 			uniformClass = "CFP_KhetPartug_Long_olive";
+			containerClass="Supply40";
+			mass=20;
+		};
+	};
+
+	class CFP_U_KhetPartug_Short_olive: CFP_U_KhetPartug_Short_M81
+	{
+		displayName = "Khet Partug (Short, Olive)";
+
+		class ItemInfo: UniformItem
+		{
+			uniformModel = "-";
+			uniformClass = "CFP_KhetPartug_Short_Olive";
+			containerClass="Supply40";
+			mass=20;
+		};
+	};
+
+	class CFP_U_KhetPartug_Long_light_olive: CFP_U_KhetPartug_Long_M81
+	{
+		displayName = "Khet Partug (Light Olive)";
+
+		class ItemInfo: UniformItem
+		{
+			uniformModel = "-";
+			uniformClass = "CFP_KhetPartug_Long_light_olive";
+			containerClass="Supply40";
+			mass=20;
+		};
+	};
+
+	class CFP_U_KhetPartug_Short_light_olive: CFP_U_KhetPartug_Short_M81
+	{
+		displayName = "Khet Partug (Short, Light Olive)";
+
+		class ItemInfo: UniformItem
+		{
+			uniformModel = "-";
+			uniformClass = "CFP_KhetPartug_Short_Light_Olive";
+			containerClass="Supply40";
+			mass=20;
+		};
+	};
+
+	class CFP_U_KhetPartug_Long_olive_polygon: CFP_U_KhetPartug_Long_M81
+	{
+		displayName = "Khet Partug (Olive / Desert Polygon)";
+
+		class ItemInfo: UniformItem
+		{
+			uniformModel = "-";
+			uniformClass = "CFP_KhetPartug_Long_olive_polygon";
+			containerClass="Supply40";
+			mass=20;
+		};
+	};
+
+	class CFP_U_KhetPartug_Short_olive_polygon: CFP_U_KhetPartug_Short_M81
+	{
+		displayName = "Khet Partug (Short, Olive / Desert Polygon)";
+
+		class ItemInfo: UniformItem
+		{
+			uniformModel = "-";
+			uniformClass = "CFP_KhetPartug_Short_olive_polygon";
 			containerClass="Supply40";
 			mass=20;
 		};
