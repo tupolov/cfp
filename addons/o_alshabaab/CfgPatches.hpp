d4e75f830e391c911bf3b0a6a502d543c65eb9aa
AUTigerGrad <audrew77@yahoo.com>
2017-11-18 22:49:36 -0500

    Added al-Shabaab faction (complete)

---
+++ addons/o_alshabaab/CfgPatches.hpp
@@ -0,0 +1,50 @@
+class CfgPatches {
+    class CFP_O_ALSHABAAB {
+        units[] = {
+            "CFP_O_ALSHABAAB_Rifleman_01",
+            "CFP_O_ALSHABAAB_Team_Leader_01",
+            "CFP_O_ALSHABAAB_Rifleman_AT_01",
+            "CFP_O_ALSHABAAB_Machine_Gunner_01",
+            "CFP_O_ALSHABAAB_Medic_01",
+            "CFP_O_ALSHABAAB_Grenadier_01",
+            "CFP_O_ALSHABAAB_Explosive_Specialist_01",
+            "CFP_O_ALSHABAAB_Sniper_01",
+            "CFP_O_ALSHABAAB_Gunner_DShKM_High_01",
+            "CFP_O_ALSHABAAB_Gunner_DShKM_Low_01",
+            "CFP_O_ALSHABAAB_Gunner_Mortar_01",
+            "CFP_O_ALSHABAAB_Gunner_SPG_01",
+            "CFP_O_ALSHABAAB_Ural_ZU_23_01",
+            "CFP_O_ALSHABAAB_Technical_PK_01",
+            "CFP_O_ALSHABAAB_Technical_01",
+            "CFP_O_ALSHABAAB_Offroad_Armed_01",
+            "CFP_O_ALSHABAAB_Offroad_01",
+            "CFP_O_ALSHABAAB_Zamak_01",
+            "CFP_O_ALSHABAAB_Truck_01",
+            "CFP_O_ALSHABAAB_DShKM_High_01",
+            "CFP_O_ALSHABAAB_DShKM_Low_01",
+            "CFP_O_ALSHABAAB_Podnos_2B14_01",
+            "CFP_O_ALSHABAAB_SPG_9_01",
+            "CFP_O_ALSHABAAB_Ural_Ammo_01",
+            "CFP_O_ALSHABAAB_Ural_Fuel_01",
+            "CFP_O_ALSHABAAB_Ural_Repair_01",
+            "CFP_O_ALSHABAAB_UAZ_SPG_01",
+            "CFP_O_ALSHABAAB_Ural_Open_01"
+        };
+        weapons[] = {};
+        requiredVersion = 1.62;
+        requiredAddons[] = {
+            "A3_Characters_F",
+            "cfp_uniforms",
+            "CUP_WheeledVehicles_Ural",
+            "CUP_WheeledVehicles_Datsun",
+            "A3_Soft_F_Offroad_01",
+            "A3_Soft_F_Exp_Offroad_01",
+            "A3_Soft_F_Beta_Truck_02",
+            "A3_Soft_F_Gamma_Van_01",
+            "CUP_Weapons_StaticWeapons",
+            "CUP_WheeledVehicles_UAZ"
+        };
+        author = "Drew";
+        authors[] = {"Drew"};
+    };
+};
---
+++ addons/o_alshabaab/script_component.hpp
@@ -0,0 +1,13 @@
+#define COMPONENT o_alshabaab
+#define COMPONENT_BEAUTIFIED o_alshabaab
+#include "\x\cfp\addons\main\script_mod.hpp"
+// #define DEBUG_MODE_FULL
+// #define DISABLE_COMPILE_CACHE
+// #define ENABLE_PERFORMANCE_COUNTERS
+#ifdef DEBUG_ENABLED_O_ALSHABAAB
+#define DEBUG_MODE_FULL
+#endif
+#ifdef DEBUG_SETTINGS_O_ALSHABAAB
+#define DEBUG_SETTINGS DEBUG_SETTINGS_O_ALSHABAAB
+#endif
+#include "\x\cfp\addons\main\script_macros.hpp"
