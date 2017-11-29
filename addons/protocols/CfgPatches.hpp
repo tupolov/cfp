6b4bafb18c3ddd03979b126a40638f3859ac1eed
Tupolov <tupolov73@gmail.com>
2017-11-29 23:26:37 +0000

    [protocols] Fix protocols CfgPatches.hpp - stop error on using ORBATRON

--- addons/protocols/CfgPatches.hpp
+++ addons/protocols/CfgPatches.hpp
@@ -6,6 +6,8 @@ class cfgPatches
         authors[] = {"AccuracyThruVolume"};
         requiredVersion = REQUIRED_VERSION;
         VERSION_CONFIG;
+        units[] = {};
+        weapons[] ={};
 		requiredAddons[]=
 		{
 			"A3_Dubbing_Radio_F"
