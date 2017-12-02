02044f2759e18bf3ba0c5fd347372dd9aaf95897
Tupolov <tupolov73@gmail.com>
2017-12-02 00:01:41 +0000

    [b_czarmy_wdl] Added ACR (Jungle / Woodland) faction from CUP. WIP.

---
+++ addons/b_czarmy_wdl/CfgPatches.hpp
@@ -0,0 +1,44 @@
+class CfgPatches {
+    class CFP_B_CZARMY_WDL {
+        units[] = {
+            "CFP_B_CZARMY_Mi_171Sh_Rockets_WDL_01",
+            "CFP_B_CZARMY_BVP_2HQ_WDL_02",
+            "CFP_B_CZARMY_BVP_2_WDL_02",
+            "CFP_B_CZARMY_HMMWV_Ambulance_WDL_01",
+            "CFP_B_CZARMY_L39ZA_WDL_01",
+            "CFP_B_CZARMY_Mi_35_WDL_01",
+            "CFP_B_CZARMY_BRDM_2HQ_WDL_01",
+            "CFP_B_CZARMY_Mi_171Sh_Unarmed_WDL_01",
+            "CFP_B_CZARMY_Mi_35_Dark_WDL_01",
+            "CFP_B_CZARMY_Mi_35_Tiger_WDL_01",
+            "CFP_B_CZARMY_Mi_35_Ram_WDL_01",
+            "CFP_B_CZARMY_HMMWV_M1151_M2_WDL_01",
+            "CFP_B_CZARMY_HMMWV_M1114_DSHKM_WDL_01",
+            "CFP_B_CZARMY_HMMWV_M1114_AGS_WDL_01",
+            "CFP_B_CZARMY_BVP_2_WDL_01",
+            "CFP_B_CZARMY_BVP_2HQ_WDL_01",
+            "CFP_B_CZARMY_Land_Rover_Special_Desert_WDL_01",
+            "CFP_B_CZARMY_BRDM_2_WDL_01",
+            "CFP_B_CZARMY_Podnos_2B14_WDL_01",
+            "CFP_B_CZARMY_AGS_30_WDL_01",
+            "CFP_B_CZARMY_DShKM_WDL_01",
+            "CFP_B_CZARMY_RBS_70_WDL_01"
+        };
+        weapons[] = {};
+        requiredAddons[] = {
+            "CUP_Creatures_People_Military_DummyInfantrySet",
+            "CUP_AirVehicles_Mi8",
+            "CUP_TrackedVehicles_BMP",
+            "CUP_WheeledVehicles_HMMWV",
+            "CUP_AirVehicles_L39",
+            "CUP_AirVehicles_Mi24",
+            "CUP_WheeledVehicles_BRDM2",
+            "CUP_WheeledVehicles_LR",
+            "CUP_Weapons_StaticWeapons"
+        };
+        author = MODULE_AUTHOR;
+        authors[] = {"CUP", "Tupolov"};
+        requiredVersion = REQUIRED_VERSION;
+        VERSION_CONFIG;
+    };
+};
---
+++ addons/b_czarmy_wdl/script_component.hpp
@@ -0,0 +1,13 @@
+#define COMPONENT b_czarmy_wdl
+#define COMPONENT_BEAUTIFIED b_czarmy_wdl
+#include "\x\cfp\addons\main\script_mod.hpp"
+// #define DEBUG_MODE_FULL
+// #define DISABLE_COMPILE_CACHE
+// #define ENABLE_PERFORMANCE_COUNTERS
+#ifdef DEBUG_ENABLED_B_CZARMY_WDL
+#define DEBUG_MODE_FULL
+#endif
+#ifdef DEBUG_SETTINGS_B_CZARMY_WDL
+#define DEBUG_SETTINGS DEBUG_SETTINGS_B_CZARMY_WDL
+#endif
+#include "\x\cfp\addons\main\script_macros.hpp"
