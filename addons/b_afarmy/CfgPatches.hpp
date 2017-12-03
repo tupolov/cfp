f91f84461be74e91ff07c71ce7e436d7273450cc
AUTigerGrad <audrew77@yahoo.com>
2017-12-02 23:04:18 -0500

    Added initial release of Afghan Army (Infantry, Motorized, Mech groups)

--- addons/b_afarmy/CfgPatches.hpp
+++ addons/b_afarmy/CfgPatches.hpp
@@ -2,13 +2,32 @@ class CfgPatches {
     class CFP_B_AFARMY {
         units[] = {
             "CFP_B_AFARMY_Rifleman_01",
-            "CFP_B_AFARMY_Commando_Rifleman_01"
+            "CFP_B_AFARMY_Squad_Leader_01",
+            "CFP_B_AFARMY_Grenadier_01",
+            "CFP_B_AFARMY_Rifleman_AT_01",
+            "CFP_B_AFARMY_Machine_Gunner_01",
+            "CFP_B_AFARMY_Medic_01",
+            "CFP_B_AFARMY_Explosive_Specialist_01",
+            "CFP_B_AFARMY_Repair_Specialist_01",
+            "CFP_B_AFARMY_Sniper_01",
+            "CFP_B_AFARMY_Officer_01",
+            "CFP_B_AFARMY_M113_01",
+            "CFP_B_AFARMY_M113_Ambulance_01",
+            "CFP_B_AFARMY_HMMWV_M2_01",
+            "CFP_B_AFARMY_HMMWV_01",
+            "CFP_B_AFARMY_MTVR_01",
+            "CFP_B_AFARMY_MTVR_Ammo_01",
+            "CFP_B_AFARMY_MTVR_Refuel_01",
+            "CFP_B_AFARMY_MTVR_Repair_01"
         };
         weapons[] = {};
         requiredVersion = 1.62;
         requiredAddons[] = {
             "A3_Characters_F",
-            "cfp_uniforms"
+            "cfp_uniforms",
+            "CUP_TrackedVehicles_M113",
+            "CUP_WheeledVehicles_UpHMMWV",
+            "CFP_B_USARMY_WDL"
         };
         author = "Drew";
         authors[] = {"Drew"};
--- addons/b_afarmy/config.cpp
+++ addons/b_afarmy/config.cpp
@@ -1,6 +1,5 @@
 #include "script_component.hpp"
 #include "CfgPatches.hpp"
 #include "CfgFactionClasses.hpp"
-#include "CfgUnitInsignias.hpp"
 #include "CfgVehicles.hpp"
 #include "CfgGroups.hpp"
