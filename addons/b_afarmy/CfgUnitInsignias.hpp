387297d88aeb6eccf7ef4e69e69899435666004a
Tupolov <tupolov73@gmail.com>
2017-12-03 10:41:48 +0000

    [b_afarmy] Revert back deleted insignias

---
+++ addons/b_afarmy/CfgUnitInsignias.hpp
@@ -0,0 +1,159 @@
+class CfgUnitInsignia
+{
+	class AFG_Flag_Afghanistan
+	{
+		displayName = "AFG Flag Afghanistan";
+		author = "SP Craig";
+		texture = "\x\cfp\addons\b_afarmy\data\AFG_Flag_Afghanistan.paa";
+		textureVehicle = "";
+		scopeArsenal = 2;
+	};
+	class AFG_Flag_Afghanistan_sub: AFG_Flag_Afghanistan
+	{
+		displayName = "AFG Flag Afghanistan sub";
+		author = "SP Craig";
+		texture = "\x\cfp\addons\b_afarmy\data\AFG_Flag_Afghanistan_sub.paa";
+		textureVehicle = "";
+	};
+
+	class AFG_Flag_KingdomOfAfghanistan: AFG_Flag_Afghanistan
+	{
+		displayName = "AFG Flag Kingdom Of Afghanistan 1926 to 1973";
+		author = "SP Craig";
+		texture = "\x\cfp\addons\b_afarmy\data\AFG_Flag_KingdomOfAfghanistan.paa";
+		textureVehicle = "";
+	};
+
+	class AFG_Flag_RepublicOfAfghanistan: AFG_Flag_Afghanistan
+	{
+		displayName = "AFG Flag Republic Of Afghanistan 1973 to 1978";
+		author = "SP Craig";
+		texture = "\x\cfp\addons\b_afarmy\data\AFG_Flag_RepublicOfAfghanistan.paa";
+		textureVehicle = "";
+	};
+
+
+	class AFG_Flag_DemocraticRepublicOfAfghanistan: AFG_Flag_Afghanistan
+	{
+		displayName = "AFG Flag Democratic Republic Of Afghanistan 1978 to 1992";
+		author = "SP Craig";
+		texture = "\x\cfp\addons\b_afarmy\data\AFG_Flag_DemocraticRepublicOfAfghanistan.paa";
+		textureVehicle = "";
+	};
+
+	class AFG_Flag_IslamicStateOfAfghanistan: AFG_Flag_Afghanistan
+	{
+		displayName = "AFG Flag Islamic State Of Afghanistan 1992 to 2001";
+		author = "SP Craig";
+		texture = "\x\cfp\addons\b_afarmy\data\AFG_Flag_IslamicStateOfAfghanistan.paa";
+		textureVehicle = "";
+	};
+
+	class AFG_Flag_IslamicEmirateOfAfghanistan: AFG_Flag_Afghanistan
+	{
+		displayName = "AFG Flag Islamic Emirate Of Afghanistan 1996 to 2001";
+		author = "SP Craig";
+		texture = "\x\cfp\addons\b_afarmy\data\AFG_Flag_IslamicEmirateOfAfghanistan.paa";
+		textureVehicle = "";
+
+
+
+	};
+
+	class AFG_NationalArmy: AFG_Flag_Afghanistan
+	{
+		displayName = "AFG National Army";
+		author = "SP Craig";
+		texture = "\x\cfp\addons\b_afarmy\data\AFG_NationalArmy.paa";
+		textureVehicle = "";
+
+
+	};
+
+	class AFG_201stCorps: AFG_Flag_Afghanistan
+	{
+		displayName = "AFG 201st Corps";
+		author = "SP Craig";
+		texture = "\x\cfp\addons\b_afarmy\data\AFG_201stCorps.paa";
+		textureVehicle = "";
+
+
+	};
+
+	class AFG_203rdCorps: AFG_Flag_Afghanistan
+	{
+		displayName = "AFG 203rd Corps";
+		author = "SP Craig";
+		texture = "\x\cfp\addons\b_afarmy\data\AFG_203rdCorps.paa";
+		textureVehicle = "";
+
+
+
+	};
+
+	class AFG_205thCorps: AFG_Flag_Afghanistan
+	{
+		displayName = "AFG 205th Corps";
+		author = "SP Craig";
+		texture = "\x\cfp\addons\b_afarmy\data\AFG_205thCorps.paa";
+		textureVehicle = "";
+
+
+	};
+
+	class AFG_207thCorps: AFG_Flag_Afghanistan
+	{
+		displayName = "AFG 207th Corps";
+		author = "SP Craig";
+		texture = "\x\cfp\addons\b_afarmy\data\AFG_207thCorps.paa";
+		textureVehicle = "";
+
+
+	};
+
+	class AFG_209thCorps: AFG_Flag_Afghanistan
+	{
+		displayName = "AFG 209th Corps";
+		author = "SP Craig";
+		texture = "\x\cfp\addons\b_afarmy\data\AFG_209thCorps.paa";
+		textureVehicle = "";
+
+
+	};
+
+	class AFG_NationalInfantry: AFG_Flag_Afghanistan
+	{
+		displayName = "AFG National Infantry";
+		author = "SP Craig";
+		texture = "\x\cfp\addons\b_afarmy\data\AFG_NationalInfantry.paa";
+		textureVehicle = "";
+
+
+	};
+
+	class AFG_NationalArmyParatrooper: AFG_Flag_Afghanistan
+	{
+		displayName = "AFG National Army Paratrooper";
+		author = "SP Craig";
+		texture = "\x\cfp\addons\b_afarmy\data\AFG_NationalArmyParatrooper.paa";
+		textureVehicle = "";
+
+
+	};
+
+	class AFG_Commandos: AFG_Flag_Afghanistan
+	{
+		displayName = "AFG Commandos";
+		author = "SP Craig";
+		texture = "\x\cfp\addons\b_afarmy\data\AFG_Commandos.paa";
+		textureVehicle = "";
+	};
+
+	class AFG_CommandoSpecialForces: AFG_Flag_Afghanistan
+	{
+		displayName = "AFG Commando Special Forces";
+		author = "SP Craig";
+		texture = "\x\cfp\addons\b_afarmy\data\AFG_CommandoSpecialForces.paa";
+		textureVehicle = "";
+	};
+};
