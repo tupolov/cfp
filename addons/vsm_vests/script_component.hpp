21d1d68299eac56ed4851e5bbaa997b5e4e2de06
Tupolov <tupolov73@gmail.com>
2018-02-25 23:33:05 +0000

    [vsm_vests] Added a bunch of very cool vests thanks and credit to VanSchmoozin. Thanks!

---
+++ addons/vsm_vests/script_component.hpp
@@ -0,0 +1,13 @@
+#define COMPONENT vsm_vests
+#define COMPONENT_BEAUTIFIED VSM_Vests
+#include "\x\cfp\addons\main\script_mod.hpp"
+
+#ifdef DEBUG_ENABLED_VSM_VESTS
+    #define DEBUG_MODE_FULL
+#endif
+
+#ifdef DEBUG_SETTINGS_VSM_VESTS
+    #define DEBUG_SETTINGS DEBUG_SETTINGS_VSM_VESTS
+#endif
+
+#include "\x\cfp\addons\main\script_macros.hpp"
