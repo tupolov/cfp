13bf76c09cc6e197e7638adcc1b1580192a2c7ec
Tupolov <tupolov73@gmail.com>
2018-03-05 17:01:46 +0000

    [vests] Update Osprey Mk2 (Woodland - Empty) config. Remove unnecessary files.

--- addons/vests/OspreyMk2/CfgWeapons.hpp
+++ addons/vests/OspreyMk2/CfgWeapons.hpp
@@ -1,61 +1,11 @@
 
-
-/*OspreyMk2
-
-class CfgVehicleClasses
-{
-	class CFP_Osprey
-	{
-		displayName = "Osprey";
-	};
-};
-*/
-
-	class CFP_Osprey1_Wd: V_PlateCarrier1_rgr
+	class CUP_V_BAF_Osprey_Mk2_DPM_Empty;
+	class CFP_Osprey1_Wd: CUP_V_BAF_Osprey_Mk2_DPM_Empty
 	{
 		scope = 2;
-		displayName = "Osprey Mk2 (Woodland)";
+		displayName = "Osprey Mk2 (Woodland - Empty)";
 		picture = "\x\cfp\addons\vests\OspreyMk2\ui\icon_v_mk2_empty_wd.paa";
-		model = "\x\cfp\addons\models\Mk2_Empty_Vest";
-		hiddenSelectionsTextures[] = {"\x\cfp\addons\vests\OspreyMk2\mk2_empty_m81.paa"};
-		hiddenSelections[] = {"Camo"};
-
-
-
-		class ItemInfo: VestItem
-		{
-			uniformModel = "\x\cfp\addons\models\Mk2_Empty_Vest";
-			containerClass = "Supply80";
-			hiddenSelections[] = {"camo"};
-			mass = 100;
-			class HitpointsProtectionInfo
-			{
-				class Chest
-				{
-					hitpointName = "HitChest";
-					armor = 10;
-					passThrough = 0.2;
-				};
-				class Diaphragm
-				{
-					hitpointName = "HitDiaphragm";
-					armor = 10;
-					passThrough = 0.2;
-				};
-				class Abdomen
-				{
-					hitpointName = "HitAbdomen";
-					armor = 10;
-					passThrough = 0.2;
-				};
-				class Body
-				{
-					hitpointName = "HitBody";
-					passThrough = 0.2;
-				};
-			};
-
-		};
+		hiddenSelectionsTextures[] = {"\x\cfp\addons\vests\OspreyMk2\mk2_empty_m81.paa","\x\cfp\addons\vests\OspreyMk2\mk2_empty_m81.paa","\x\cfp\addons\vests\OspreyMk2\mk2_empty_m81.paa","\x\cfp\addons\vests\OspreyMk2\mk2_empty_m81.paa"};
 	};
 
 
