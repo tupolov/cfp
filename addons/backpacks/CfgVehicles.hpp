df4687b6cdc93a8f48b14409ebea0da3f49ffce0
Tupolov <tupolov73@gmail.com>
2018-04-05 11:58:45 +0100

    [ui] Add icons for all CFP backpacks

--- addons/backpacks/CfgVehicles.hpp
+++ addons/backpacks/CfgVehicles.hpp
@@ -3,286 +3,315 @@ class CfgVehicles
     class B_Carryall_oucamo;
     class SP_Carryall_Black: B_Carryall_oucamo
     {
+        picture = "\x\cfp\addons\backpacks\data\ui\icon_SP_Carryall_Black.paa";
         scope = 2;
         maximumLoad = 300;
         displayName = CSTRING(SP_Carryall_Black);
-        picture = "\x\cfp\addons\backpacks\data\icon_Black.jpg";
+        // picture = "\x\cfp\addons\backpacks\data\icon_Black.jpg";
         hiddenSelectionsTextures[] = {"\x\cfp\addons\backpacks\data\Black.paa"};
         author = "SP Craig";
     };
     class SP_Carryall_Green: B_Carryall_oucamo
     {
+        picture = "\x\cfp\addons\backpacks\data\ui\icon_SP_Carryall_Green.paa";
         scope = 2;
         maximumLoad = 300;
         displayName = CSTRING(SP_Carryall_Green);
-        picture = "\x\cfp\addons\backpacks\data\icon_Green.jpg";
+        // picture = "\x\cfp\addons\backpacks\data\icon_Green.jpg";
         hiddenSelectionsTextures[] = {"\x\cfp\addons\backpacks\data\Green.paa"};
         author = "SP Craig";
     };
     class SP_Carryall_Tan: B_Carryall_oucamo
     {
+        picture = "\x\cfp\addons\backpacks\data\ui\icon_SP_Carryall_Tan.paa";
         scope = 2;
         maximumLoad = 300;
         displayName = CSTRING(SP_Carryall_Tan);
-        picture = "\x\cfp\addons\backpacks\data\icon_Tan.jpg";
+        // picture = "\x\cfp\addons\backpacks\data\icon_Tan.jpg";
         hiddenSelectionsTextures[] = {"\x\cfp\addons\backpacks\data\Tan.paa"};
         author = "SP Craig";
     };
     class SP_Carryall_White: B_Carryall_oucamo
     {
+        picture = "\x\cfp\addons\backpacks\data\ui\icon_SP_Carryall_White.paa";
         scope = 2;
         maximumLoad = 300;
         displayName = CSTRING(SP_Carryall_White);
-        picture = "\x\cfp\addons\backpacks\data\White.jpg";
+        // picture = "\x\cfp\addons\backpacks\data\White.jpg";
         hiddenSelectionsTextures[] = {"\x\cfp\addons\backpacks\data\White.paa"};
         author = "SP Craig";
     };
     class SP_Carryall_Hunter: B_Carryall_oucamo
     {
+        picture = "\x\cfp\addons\backpacks\data\ui\icon_SP_Carryall_Hunter.paa";
         scope = 2;
         maximumLoad = 300;
         displayName = CSTRING(SP_Carryall_Hunter);
-        picture = "\x\cfp\addons\backpacks\data\Hunter.jpg";
+        // picture = "\x\cfp\addons\backpacks\data\Hunter.jpg";
         hiddenSelectionsTextures[] = {"\x\cfp\addons\backpacks\data\Hunter.paa"};
         author = "SP Craig";
     };
     class SP_Carryall_Hunter2: B_Carryall_oucamo
     {
+        picture = "\x\cfp\addons\backpacks\data\ui\icon_SP_Carryall_Hunter2.paa";
         scope = 2;
         maximumLoad = 300;
         displayName = CSTRING(SP_Carryall_Hunter2);
-        picture = "\x\cfp\addons\backpacks\data\Hunter2.jpg";
+        // picture = "\x\cfp\addons\backpacks\data\Hunter2.jpg";
         hiddenSelectionsTextures[] = {"\x\cfp\addons\backpacks\data\Hunter2.paa"};
         author = "SP Craig";
     };
     class SP_Carryall_ATacsFG: B_Carryall_oucamo
     {
+        picture = "\x\cfp\addons\backpacks\data\ui\icon_SP_Carryall_ATacsFG.paa";
         scope = 2;
         maximumLoad = 300;
         displayName = CSTRING(SP_Carryall_ATacsFG);
-        picture = "\x\cfp\addons\backpacks\data\ATacsFG.jpg";
+        // picture = "\x\cfp\addons\backpacks\data\ATacsFG.jpg";
         hiddenSelectionsTextures[] = {"\x\cfp\addons\backpacks\data\ATacsFG.paa"};
         author = "SP Craig";
     };
 	class Carryall_ATacsAU: B_Carryall_oucamo
     {
+        picture = "\x\cfp\addons\backpacks\data\ui\icon_Carryall_ATacsAU.paa";
         scope = 2;
         maximumLoad = 300;
         displayName = CSTRING(SP_Carryall_ATacsAU);
-        picture = "\x\cfp\addons\backpacks\data\icon_ATacsAU.jpg";
+        // picture = "\x\cfp\addons\backpacks\data\icon_ATacsAU.jpg";
         hiddenSelectionsTextures[] = {"\x\cfp\addons\backpacks\data\ATacsAU.paa"};
         author = "AccuracyThruVolume";
     };
     class SP_Carryall_NodUrban: B_Carryall_oucamo
     {
+        picture = "\x\cfp\addons\backpacks\data\ui\icon_SP_Carryall_NodUrban.paa";
         scope = 2;
         maximumLoad = 300;
         displayName = CSTRING(SP_Carryall_NodUrban);
-        picture = "\x\cfp\addons\backpacks\data\NodUrban.jpg";
+        // picture = "\x\cfp\addons\backpacks\data\NodUrban.jpg";
         hiddenSelectionsTextures[] = {"\x\cfp\addons\backpacks\data\NodUrban.paa"};
         author = "SP Craig";
     };
     class SP_Camelbak_Black: B_Carryall_oucamo
     {
+        picture = "\x\cfp\addons\backpacks\data\ui\icon_SP_Camelbak_Black.paa";
         scope = 2;
         maximumLoad = 50;
         displayName = "Camelbak (Black)";
         model = "\x\cfp\addons\backpacks\Camelbak.p3d";
-        picture = "\x\cfp\addons\backpacks\data\Black.jpg";
+        // picture = "\x\cfp\addons\backpacks\data\Black.jpg";
         hiddenSelectionsTextures[] = {"\x\cfp\addons\backpacks\data\Black.paa"};
         author = "SP Craig";
     };
     class SP_Camelbak_Green: B_Carryall_oucamo
     {
+        picture = "\x\cfp\addons\backpacks\data\ui\icon_SP_Camelbak_Green.paa";
         scope = 2;
         maximumLoad = 50;
         displayName = "Camelbak (Green)";
         model = "\x\cfp\addons\backpacks\Camelbak.p3d";
-        picture = "\x\cfp\addons\backpacks\data\Green.jpg";
+        // picture = "\x\cfp\addons\backpacks\data\Green.jpg";
         hiddenSelectionsTextures[] = {"\x\cfp\addons\backpacks\data\Green.paa"};
         author = "SP Craig";
     };
     class SP_Camelbak_Tan: B_Carryall_oucamo
     {
+        picture = "\x\cfp\addons\backpacks\data\ui\icon_SP_Camelbak_Tan.paa";
         scope = 2;
         maximumLoad = 50;
         displayName = "Camelbak (Tan)";
         model = "\x\cfp\addons\backpacks\Camelbak.p3d";
-        picture = "\x\cfp\addons\backpacks\data\Tan.jpg";
+        // picture = "\x\cfp\addons\backpacks\data\Tan.jpg";
         hiddenSelectionsTextures[] = {"\x\cfp\addons\backpacks\data\Tan.paa"};
         author = "SP Craig";
     };
     class CUP_B_RPGPack_Base;
-    class CFP_RPGPack_Black : CUP_B_RPGPack_Base
+    class CFP_RPGPack_Black: CUP_B_RPGPack_Base
     {
+        picture = "\x\cfp\addons\backpacks\data\ui\icon_CFP_RPGPack_Black.paa";
         scope = 2;
         maximumLoad = 180;
         displayName = "RPG Pack (Black)";
         model = "\CUP\Weapons\CUP_Weapons_Backpacks\CUP_Backpack_RPG.p3d";
-        picture = "\CUP\Weapons\CUP_Weapons_Backpacks\data\ui\icon_b_c_backpack_rpg_ca.paa";
+        // picture = "\CUP\Weapons\CUP_Weapons_Backpacks\data\ui\icon_b_c_backpack_rpg_ca.paa";
         hiddenSelectionsTextures[] = {"\x\cfp\addons\backpacks\RPG\RPGblack.paa"};
         author = "AuburnAlumni";
     };
-    class CFP_RPGPack_Grey : CUP_B_RPGPack_Base
+    class CFP_RPGPack_Grey: CUP_B_RPGPack_Base
     {
+        picture = "\x\cfp\addons\backpacks\data\ui\icon_CFP_RPGPack_Grey.paa";
         scope = 2;
         maximumLoad = 180;
         displayName = "RPG Pack (Grey)";
         model = "\CUP\Weapons\CUP_Weapons_Backpacks\CUP_Backpack_RPG.p3d";
-        picture = "\CUP\Weapons\CUP_Weapons_Backpacks\data\ui\icon_b_c_backpack_rpg_ca.paa";
+        // picture = "\CUP\Weapons\CUP_Weapons_Backpacks\data\ui\icon_b_c_backpack_rpg_ca.paa";
         hiddenSelectionsTextures[] = {"\x\cfp\addons\backpacks\RPG\RPGgrey.paa"};
         author = "AuburnAlumni";
     };
-    class CFP_RPGPack_Khaki : CUP_B_RPGPack_Base
+    class CFP_RPGPack_Khaki: CUP_B_RPGPack_Base
     {
+        picture = "\x\cfp\addons\backpacks\data\ui\icon_CFP_RPGPack_Khaki.paa";
         scope = 2;
         maximumLoad = 180;
         displayName = "RPG Pack (Khaki)";
         model = "\CUP\Weapons\CUP_Weapons_Backpacks\CUP_Backpack_RPG.p3d";
-        picture = "\CUP\Weapons\CUP_Weapons_Backpacks\data\ui\icon_b_c_backpack_rpg_ca.paa";
+        // picture = "\CUP\Weapons\CUP_Weapons_Backpacks\data\ui\icon_b_c_backpack_rpg_ca.paa";
         hiddenSelectionsTextures[] = {"\x\cfp\addons\backpacks\RPG\RPGkhaki.paa"};
         author = "AuburnAlumni";
     };
-    class CFP_RPGPack_TropicGreen : CUP_B_RPGPack_Base
+    class CFP_RPGPack_TropicGreen: CUP_B_RPGPack_Base
     {
+        picture = "\x\cfp\addons\backpacks\data\ui\icon_CFP_RPGPack_TropicGreen.paa";
         scope = 2;
         maximumLoad = 180;
         displayName = "RPG Pack (Tropical Green)";
         model = "\CUP\Weapons\CUP_Weapons_Backpacks\CUP_Backpack_RPG.p3d";
-        picture = "\CUP\Weapons\CUP_Weapons_Backpacks\data\ui\icon_b_c_backpack_rpg_ca.paa";
+        // picture = "\CUP\Weapons\CUP_Weapons_Backpacks\data\ui\icon_b_c_backpack_rpg_ca.paa";
         hiddenSelectionsTextures[] = {"\x\cfp\addons\backpacks\RPG\RPGtropicgreen.paa"};
         author = "AuburnAlumni";
     };
     class B_Kitbag_Base;
-    class CFP_Kitbag_EDRL : B_Kitbag_Base
+    class CFP_Kitbag_EDRL: B_Kitbag_Base
     {
+        picture = "\x\cfp\addons\backpacks\data\ui\icon_CFP_Kitbag_EDRL.paa";
         scope = 2;
         maximumLoad = 280;
         displayName = "Kitbag (EDRL)";
         model = "\A3\weapons_f\Ammoboxes\bags\Backpack_Fast.p3d";
-        picture = "\A3\Weapons_F\ammoboxes\bags\data\ui\icon_B_C_Kitbag_rgr";
+        // picture = "\A3\Weapons_F\ammoboxes\bags\data\ui\icon_B_C_Kitbag_rgr";
         hiddenSelectionsTextures[] = {"\x\cfp\addons\backpacks\Fast\backpack_fast_edrl.paa"};
         author = "AuburnAlumni";
     };
-    class CFP_Kitbag_IranDPM : B_Kitbag_Base
+    class CFP_Kitbag_IranDPM: B_Kitbag_Base
     {
+        picture = "\x\cfp\addons\backpacks\data\ui\icon_CFP_Kitbag_IranDPM.paa";
         scope = 2;
         maximumLoad = 280;
         displayName = "Kitbag (Iran DPM)";
         model = "\A3\weapons_f\Ammoboxes\bags\Backpack_Fast.p3d";
-        picture = "\A3\Weapons_F\ammoboxes\bags\data\ui\icon_B_C_Kitbag_rgr";
+        // picture = "\A3\Weapons_F\ammoboxes\bags\data\ui\icon_B_C_Kitbag_rgr";
         hiddenSelectionsTextures[] = {"\x\cfp\addons\backpacks\Fast\backpack_fast_irandpm.paa"};
         author = "AuburnAlumni";
     };
-    class CFP_Kitbag_M81 : B_Kitbag_Base
+    class CFP_Kitbag_M81: B_Kitbag_Base
     {
+        picture = "\x\cfp\addons\backpacks\data\ui\icon_CFP_Kitbag_M81.paa";
         scope = 2;
         maximumLoad = 280;
         displayName = "Kitbag (M81)";
         model = "\A3\weapons_f\Ammoboxes\bags\Backpack_Fast.p3d";
-        picture = "\A3\Weapons_F\ammoboxes\bags\data\ui\icon_B_C_Kitbag_rgr";
+        // picture = "\A3\Weapons_F\ammoboxes\bags\data\ui\icon_B_C_Kitbag_rgr";
         hiddenSelectionsTextures[] = {"\x\cfp\addons\backpacks\Fast\backpack_fast_m81.paa"};
         author = "AuburnAlumni";
     };
-    class CFP_Kitbag_Marpat : B_Kitbag_Base
+    class CFP_Kitbag_Marpat: B_Kitbag_Base
     {
+        picture = "\x\cfp\addons\backpacks\data\ui\icon_CFP_Kitbag_Marpat.paa";
         scope = 2;
         maximumLoad = 280;
         displayName = "Kitbag (Marpat)";
         model = "\A3\weapons_f\Ammoboxes\bags\Backpack_Fast.p3d";
-        picture = "\A3\Weapons_F\ammoboxes\bags\data\ui\icon_B_C_Kitbag_rgr";
+        // picture = "\A3\Weapons_F\ammoboxes\bags\data\ui\icon_B_C_Kitbag_rgr";
         hiddenSelectionsTextures[] = {"\x\cfp\addons\backpacks\Fast\backpack_fast_marpat.paa"};
         author = "AuburnAlumni";
     };
-    class CFP_Kitbag_PolygonDesert : B_Kitbag_Base
+    class CFP_Kitbag_PolygonDesert: B_Kitbag_Base
     {
+        picture = "\x\cfp\addons\backpacks\data\ui\icon_CFP_Kitbag_PolygonDesert.paa";
         scope = 2;
         maximumLoad = 280;
         displayName = "Kitbag (Polygon Desert)";
         model = "\A3\weapons_f\Ammoboxes\bags\Backpack_Fast.p3d";
-        picture = "\A3\Weapons_F\ammoboxes\bags\data\ui\icon_B_C_Kitbag_rgr";
+        // picture = "\A3\Weapons_F\ammoboxes\bags\data\ui\icon_B_C_Kitbag_rgr";
         hiddenSelectionsTextures[] = {"\x\cfp\addons\backpacks\Fast\backpack_fast_polygondesert.paa"};
         author = "AuburnAlumni";
     };
-    class CFP_Kitbag_PolygonWoodland : B_Kitbag_Base
+    class CFP_Kitbag_PolygonWoodland: B_Kitbag_Base
     {
+        picture = "\x\cfp\addons\backpacks\data\ui\icon_CFP_Kitbag_PolygonWoodland.paa";
         scope = 2;
         maximumLoad = 280;
         displayName = "Kitbag (Polygon Woodland)";
         model = "\A3\weapons_f\Ammoboxes\bags\Backpack_Fast.p3d";
-        picture = "\A3\Weapons_F\ammoboxes\bags\data\ui\icon_B_C_Kitbag_rgr";
+        // picture = "\A3\Weapons_F\ammoboxes\bags\data\ui\icon_B_C_Kitbag_rgr";
         hiddenSelectionsTextures[] = {"\x\cfp\addons\backpacks\Fast\backpack_fast_polygonwoodland.paa"};
         author = "AuburnAlumni";
     };
-    class CFP_Kitbag_Woodland : B_Kitbag_Base
+    class CFP_Kitbag_Woodland: B_Kitbag_Base
     {
+        picture = "\x\cfp\addons\backpacks\data\ui\icon_CFP_Kitbag_Woodland.paa";
         scope = 2;
         maximumLoad = 280;
         displayName = "Kitbag (Woodland)";
         model = "\A3\weapons_f\Ammoboxes\bags\Backpack_Fast.p3d";
-        picture = "\A3\Weapons_F\ammoboxes\bags\data\ui\icon_B_C_Kitbag_rgr";
+        // picture = "\A3\Weapons_F\ammoboxes\bags\data\ui\icon_B_C_Kitbag_rgr";
         hiddenSelectionsTextures[] = {"\x\cfp\addons\backpacks\Fast\backpack_fast_woodland.paa"};
         author = "AuburnAlumni";
     };
     class B_AssaultPack_Base;
-    class CFP_AssaultPack_M81 : B_AssaultPack_Base
+    class CFP_AssaultPack_M81: B_AssaultPack_Base
     {
+        picture = "\x\cfp\addons\backpacks\data\ui\icon_CFP_AssaultPack_M81.paa";
         scope = 2;
         maximumLoad = 160;
         displayName = "Assault Pack (M81)";
         model = "\A3\weapons_f\Ammoboxes\bags\Backpack_Compact";
-        picture = "\A3\Weapons_F\Ammoboxes\Bags\data\UI\icon_B_AssaultPack_khk_ca.paa";
+        // picture = "\A3\Weapons_F\Ammoboxes\Bags\data\UI\icon_B_AssaultPack_khk_ca.paa";
         hiddenSelectionsTextures[] = {"\x\cfp\addons\backpacks\Compact\backpack_compact_m81.paa"};
         author = "AuburnAlumni";
     };
-    class CFP_AssaultPack_Marpat : B_AssaultPack_Base
+    class CFP_AssaultPack_Marpat: B_AssaultPack_Base
     {
+        picture = "\x\cfp\addons\backpacks\data\ui\icon_CFP_AssaultPack_Marpat.paa";
         scope = 2;
         maximumLoad = 160;
         displayName = "Assault Pack (Marpat)";
         model = "\A3\weapons_f\Ammoboxes\bags\Backpack_Compact";
-        picture = "\A3\Weapons_F\Ammoboxes\Bags\data\UI\icon_B_AssaultPack_khk_ca.paa";
+        // picture = "\A3\Weapons_F\Ammoboxes\Bags\data\UI\icon_B_AssaultPack_khk_ca.paa";
         hiddenSelectionsTextures[] = {"\x\cfp\addons\backpacks\Compact\backpack_compact_marpat.paa"};
         author = "AuburnAlumni";
     };
-    class CFP_AssaultPack_PolygonDesert : B_AssaultPack_Base
+    class CFP_AssaultPack_PolygonDesert: B_AssaultPack_Base
     {
+        picture = "\x\cfp\addons\backpacks\data\ui\icon_CFP_AssaultPack_PolygonDesert.paa";
         scope = 2;
         maximumLoad = 160;
         displayName = "Assault Pack (Polygon Desert)";
         model = "\A3\weapons_f\Ammoboxes\bags\Backpack_Compact";
-        picture = "\A3\Weapons_F\Ammoboxes\Bags\data\UI\icon_B_AssaultPack_khk_ca.paa";
+        // picture = "\A3\Weapons_F\Ammoboxes\Bags\data\UI\icon_B_AssaultPack_khk_ca.paa";
         hiddenSelectionsTextures[] = {"\x\cfp\addons\backpacks\Compact\backpack_compact_polygondesert.paa"};
         author = "AuburnAlumni";
     };
-    class CFP_AssaultPack_PolygonWoodland : B_AssaultPack_Base
+    class CFP_AssaultPack_PolygonWoodland: B_AssaultPack_Base
     {
+        picture = "\x\cfp\addons\backpacks\data\ui\icon_CFP_AssaultPack_PolygonWoodland.paa";
         scope = 2;
         maximumLoad = 160;
         displayName = "Assault Pack (Polygon Woodland)";
         model = "\A3\weapons_f\Ammoboxes\bags\Backpack_Compact";
-        picture = "\A3\Weapons_F\Ammoboxes\Bags\data\UI\icon_B_AssaultPack_khk_ca.paa";
+        // picture = "\A3\Weapons_F\Ammoboxes\Bags\data\UI\icon_B_AssaultPack_khk_ca.paa";
         hiddenSelectionsTextures[] = {"\x\cfp\addons\backpacks\Compact\backpack_compact_polygonwoodland.paa"};
         author = "AuburnAlumni";
     };
-    class CFP_AssaultPack_Woodland : B_AssaultPack_Base
+    class CFP_AssaultPack_Woodland: B_AssaultPack_Base
     {
+        picture = "\x\cfp\addons\backpacks\data\ui\icon_CFP_AssaultPack_Woodland.paa";
         scope = 2;
         maximumLoad = 160;
         displayName = "Assault Pack (Woodland)";
         model = "\A3\weapons_f\Ammoboxes\bags\Backpack_Compact";
-        picture = "\A3\Weapons_F\Ammoboxes\Bags\data\UI\icon_B_AssaultPack_khk_ca.paa";
+        // picture = "\A3\Weapons_F\Ammoboxes\Bags\data\UI\icon_B_AssaultPack_khk_ca.paa";
         hiddenSelectionsTextures[] = {"\x\cfp\addons\backpacks\Compact\backpack_compact_woodland.paa"};
         author = "AuburnAlumni";
     };
     class B_TacticalPack_Base;
-    class CFP_TacticalPack_DPMWoodland : B_TacticalPack_Base
+    class CFP_TacticalPack_DPMWoodland: B_TacticalPack_Base
     {
+        picture = "\x\cfp\addons\backpacks\data\ui\icon_CFP_TacticalPack_DPMWoodland.paa";
         scope = 2;
         maximumLoad = 240;
         displayName = "Tactical Pack (DPM Woodland)";
         model = "\A3\weapons_f\Ammoboxes\bags\Backpack_Small";
-        picture = "\A3\weapons_f\ammoboxes\bags\data\ui\icon_B_C_Small_blk.paa";
+        // picture = "\A3\weapons_f\ammoboxes\bags\data\ui\icon_B_C_Small_blk.paa";
         hiddenSelectionsTextures[] = {"\x\cfp\addons\backpacks\Tactical\tortila_dpmwoodland.paa"};
         author = "AuburnAlumni";
     };
