5e7efd01bdce6c46711b2d504bb7cc7629cb6abf
accuracythruvolume <32960729+accuracythruvolume@users.noreply.github.com>
2018-04-12 22:13:03 -0700

    Ackbars re-ackbarring again

--- addons/i_is/CfgVehicles.hpp
+++ addons/i_is/CfgVehicles.hpp
@@ -128,7 +128,7 @@ class CfgVehicles
         {
             class ADDON
             {
-                init = "(_this select 0) call CFP_main_fnc_randomizeUnit; (_this select 0) spawn CFP_i_is_fnc_randomizeUniform; [_this select 0, 'Male01_ackbar'] remoteExecCall ['setSpeaker', 0];"; //
+                init = "(_this select 0) call CFP_main_fnc_randomizeUnit; (_this select 0) spawn CFP_i_is_fnc_randomizeUniform; [_this select 0, 'Male01_ackbar'] remoteExecCall ['setSpeaker', 0];    (_this select 0) execVM ""x\cfp\addons\o_is\Scripts\SetSpeaker.sqf"";";
             };
         };
         editorPreview = "x\cfp\addons\uniforms\FieldUniform\IS\UI\Preview_is_infantry.jpg";
