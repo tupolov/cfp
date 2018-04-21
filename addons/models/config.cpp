b33b94054f6fc0b6cdb0e70d93aee9bbfc85836b
Tupolov <tupolov73@gmail.com>
2018-04-21 15:37:38 +0100

    [models] Close source models

--- addons/models/config.cpp
+++ addons/models/config.cpp
@@ -1,16 +1,24 @@
-#include "script_component.hpp"
+////////////////////////////////////////////////////////////////////
+//DeRap: cfp_models\config.bin
+//Produced from mikero's Dos Tools Dll version 6.44
+//'now' is Fri Apr 20 13:19:44 2018 : 'file' last modified on Sun Apr 15 22:46:23 2018
+//http://dev-heaven.net/projects/list_files/mikero-pbodll
+////////////////////////////////////////////////////////////////////
+
+#define _ARMA_
 
 class CfgPatches
 {
-	class ADDON
+	class cfp_models
 	{
 		units[] = {};
 		weapons[] = {};
-        author = MODULE_AUTHOR;
-        authors[] = {"SP Craig", "Tupolov"};
-        requiredVersion = REQUIRED_VERSION;
-        requiredAddons[] = {"CFP_main"};
-        VERSION_CONFIG;
+		author = "CFP Team";
+		authors[] = {"SP Craig","Tupolov"};
+		requiredVersion = 1.8;
+		requiredAddons[] = {"CFP_main"};
+		version = "0.2.5.18040601";
+		versionStr = "0.2.5.18040601";
+		versionAr[] = {0,2,5,18040601};
 	};
 };
-
