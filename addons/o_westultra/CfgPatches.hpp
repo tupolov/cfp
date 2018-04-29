bcd2d773c39a69fd7595c82151a4f67b5f017ffd
Tupolov <tupolov73@gmail.com>
2018-04-29 23:53:17 +0100

    [o_westultra] Add initial Western Ultranationalists (based on US 3 percenters).

---
+++ addons/o_westultra/CfgPatches.hpp
@@ -0,0 +1,35 @@
+class CfgPatches {
+    class CFP_O_WestUltra {
+        units[] = {
+            "CFP_O_WestUltra_Offroad_01",
+            "CFP_O_WestUltra_Rifleman_01",
+            "CFP_O_WestUltra_Team_Leader_01",
+            "CFP_O_WestUltra_Marksman_01",
+            "CFP_O_WestUltra_Gunner_01",
+            "CFP_O_WestUltra_Medic_01",
+            "CFP_O_WestUltra_Specialist_01",
+            "CFP_O_WestUltra_SUV_02",
+            "CFP_O_WestUltra_4WD_01",
+            "CFP_O_WestUltra_Quad_Bike_01",
+            "CFP_O_WestUltra_Truck_01",
+            "CFP_O_WestUltra_Van_01",
+            "CFP_O_WestUltra_Car_01"
+        };
+        weapons[] = {};
+        requiredVersion = 1.62;
+        requiredAddons[] = {
+            "CUP_Creatures_Military_Chedaki",
+            "CUP_Wheeled_SUV",
+            "A3_Soft_F_Exp_Offroad_02",
+            "A3_Sounds_F",
+            "A3_Soft_F_Quadbike_01",
+            "A3_Soft_F_Gamma_Van_01",
+            "A3_Soft_F_Orange_Van_02",
+            "CUP_WheeledVehicles_T810",
+            "A3_Soft_F",
+            "A3_Soft_F_Gamma_Hatchback_01"
+        };
+        author = "Matt";
+        authors[] = {"Matt"};
+    };
+};
