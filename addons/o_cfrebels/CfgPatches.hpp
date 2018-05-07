a6a88e74a0487a69d2c86c59dcd2cbcc12369c68
Tupolov <tupolov73@gmail.com>
2018-05-07 19:51:30 +0100

    [o_cfrebels] Initial commit for Central African Rebels by Jarrad96.

---
+++ addons/o_cfrebels/CfgPatches.hpp
@@ -0,0 +1,52 @@
+class CfgPatches {
+    class CFP_O_CFRebels {
+        units[] = {
+            "CFP_O_CFRebels_Rifleman_01",
+            "CFP_O_CFRebels_Militiaman_01",
+            "CFP_O_CFRebels_Autorifleman_RPK_01",
+            "CFP_O_CFRebels_Team_Leader_01",
+            "CFP_O_CFRebels_Surgeon_01",
+            "CFP_O_CFRebels_Demo_Specialist_01",
+            "CFP_O_CFRebels_Grenadier_01",
+            "CFP_O_CFRebels_Squad_Leader_01",
+            "CFP_O_CFRebels_RPG_Gunner_01",
+            "CFP_O_CFRebels_Sharpshooter_01",
+            "CFP_O_CFRebels_Sniper_01",
+            "CFP_O_CFRebels_Machinegunner_PKM_01",
+            "CFP_O_CFRebels_Commander_01",
+            "CFP_O_CFRebels_Ex_FACA_Rifleman_01",
+            "CFP_O_CFRebels_Ex_FACA_Autorifleman_01",
+            "CFP_O_CFRebels_Ex_FACA_Team_Leader_01",
+            "CFP_O_CFRebels_Technical_PK_01",
+            "CFP_O_CFRebels_Offroad_Repair_01",
+            "CFP_O_CFRebels_Offroad_50_Cal_01",
+            "CFP_O_CFRebels_Offroad_SPG_9_01",
+            "CFP_O_CFRebels_Truck_01",
+            "CFP_O_CFRebels_SPG_9_01",
+            "CFP_O_CFRebels_Podnos_Mortar_01",
+            "CFP_O_CFRebels_Assault_Boat_01",
+            "CFP_O_CFRebels_Gunner_HMG_01",
+            "CFP_O_CFRebels_Assistant_Gunner_HMG_01",
+            "CFP_O_CFRebels_Gunner_Mortar_01",
+            "CFP_O_CFRebels_Assistant_Gunner_Mortar_01"
+        };
+        weapons[] = {};
+        requiredAddons[] = {
+            "A3_Characters_F",
+            "cfp_uniforms",
+            "CFP_O_SOREBEL",
+            "CFP_O_ALQAEDA",
+            "CFP_O_BOKOHARAM",
+            "CFP_O_TBAN",
+            "A3_Soft_F_Offroad_01",
+            "CFP_O_ALSHABAAB",
+            "CFP_O_HEZBOLLAH",
+            "CFP_B_AFARMY",
+            "A3_Boat_F_Gamma_Boat_Transport_01"
+        };
+        authors[] = {"jarrad96"};
+        requiredVersion = REQUIRED_VERSION;
+        VERSION_CONFIG;
+        author = MODULE_AUTHOR;
+    };
+};
---
+++ addons/o_cfrebels/script_component.hpp
@@ -0,0 +1,13 @@
+#define COMPONENT o_cfrebels
+#define COMPONENT_BEAUTIFIED o_cfrebels
+#include "\x\cfp\addons\main\script_mod.hpp"
+// #define DEBUG_MODE_FULL
+// #define DISABLE_COMPILE_CACHE
+// #define ENABLE_PERFORMANCE_COUNTERS
+#ifdef DEBUG_ENABLED_O_CFREBELS
+#define DEBUG_MODE_FULL
+#endif
+#ifdef DEBUG_SETTINGS_O_CFREBELS
+#define DEBUG_SETTINGS DEBUG_SETTINGS_O_CFREBELS
+#endif
+#include "\x\cfp\addons\main\script_macros.hpp"
