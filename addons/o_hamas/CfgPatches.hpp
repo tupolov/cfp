ad67f82f269a3c6aff751727c7535a3765019606
AUTigerGrad <audrew77@yahoo.com>
2018-06-06 23:12:05 -0400

    Added Hamas Al Qassam Brigades Faction (will add groups, crates, resize pics)

---
+++ addons/o_hamas/CfgPatches.hpp
@@ -0,0 +1,49 @@
+class CfgPatches {
+    class CFP_O_HAMAS {
+        units[] = {
+            "CFP_O_HAMAS_Rifleman_01",
+            "CFP_O_HAMAS_Rifleman_AT_01",
+            "CFP_O_HAMAS_Team_Leader_01",
+            "CFP_O_HAMAS_Medic_01",
+            "CFP_O_HAMAS_Grenadier_01",
+            "CFP_O_HAMAS_Machine_Gunner_01",
+            "CFP_O_HAMAS_Explosive_Specialist_01",
+            "CFP_O_HAMAS_Repair_Specialist_01",
+            "CFP_O_HAMAS_Sniper_01",
+            "CFP_O_HAMAS_Gunner_DShKM_01",
+            "CFP_O_HAMAS_Asst_Gunner_DShKM_01",
+            "CFP_O_HAMAS_Gunner_Mortar_01",
+            "CFP_O_HAMAS_Asst_Gunner_Mortar_01",
+            "CFP_O_HAMAS_Gunner_SPG_01",
+            "CFP_O_HAMAS_Asst_Gunner_SPG_01",
+            "CFP_O_HAMAS_Gunner_Metis_01",
+            "CFP_O_HAMAS_Asst_Gunner_Metis_01",
+            "CFP_O_HAMAS_DShKM_01",
+            "CFP_O_HAMAS_Podnos_2B14_01",
+            "CFP_O_HAMAS_SPG_9_01",
+            "CFP_O_HAMAS_Metis_01",
+            "CFP_O_HAMAS_Rifleman_AA_01",
+            "CFP_O_HAMAS_Technical_MG_01",
+            "CFP_O_HAMAS_Technical_SPG_01",
+            "CFP_O_HAMAS_Technical_Metis_01",
+            "CFP_O_HAMAS_Ural_ZU_23_01",
+            "CFP_O_HAMAS_Technical_Unarmed_01",
+            "CFP_O_HAMAS_Truck_01"
+        };
+        weapons[] = {};
+        requiredVersion = 1.62;
+        requiredAddons[] = {
+            "A3_Characters_F",
+            "cfp_uniforms",
+            "CUP_Weapons_StaticWeapons",
+            "A3_Soft_F_Offroad_01",
+            "CUP_WheeledVehicles_UAZ",
+            "cfp_vehicles",
+            "CUP_WheeledVehicles_Ural",
+            "A3_Soft_F_Gamma_Van_01",
+            "A3_Soft_F_Bootcamp_Van_01"
+        };
+        author = "Drew";
+        authors[] = {"Drew"};
+    };
+};
---
+++ addons/o_hamas/script_component.hpp
@@ -0,0 +1,13 @@
+#define COMPONENT cfp_o_hamas
+#define COMPONENT_BEAUTIFIED cfp_o_hamas
+#include "\x\cfp\addons\main\script_mod.hpp"
+// #define DEBUG_MODE_FULL
+// #define DISABLE_COMPILE_CACHE
+// #define ENABLE_PERFORMANCE_COUNTERS
+#ifdef DEBUG_ENABLED_CFP_O_HAMAS
+#define DEBUG_MODE_FULL
+#endif
+#ifdef DEBUG_SETTINGS_CFP_O_HAMAS
+#define DEBUG_SETTINGS DEBUG_SETTINGS_CFP_O_HAMAS
+#endif
+#include "\x\cfp\addons\main\script_macros.hpp"
