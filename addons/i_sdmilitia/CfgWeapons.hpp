dd3c702d6db671ec012250f3d4e2a666c5ff728d
Tupolov <tupolov73@gmail.com>
2018-06-19 22:53:21 +0100

    [i_sdmilita] Added Sudan Rapid Support Forces and Janjaweed militias

---
+++ addons/i_sdmilitia/CfgWeapons.hpp
@@ -0,0 +1,38 @@
+#define HITPOINTS_HEAD(ARMOUR,PT) \
+        class HitpointsProtectionInfo \
+        { \
+            class Head \
+            { \
+                hitpointName = "HitHead"; \
+                armor = ARMOUR; \
+                passThrough = PT; \
+            }; \
+        };
+
+class cfgWeapons
+{
+	class UniformItem;
+	class VestItem;
+	class ItemCore;
+	class HeadgearItem;
+	class CFP_Uniform_RSF_01: ItemCore
+	{
+		scope=2;
+		scopeArsenal=2;
+		author="Archduke";
+		displayName="Sudan RSF Uniform (3 Color)";
+		allowedSlots[]=
+		{
+			"BACKPACK_SLOT"
+		};
+		picture="\x\addons\cfp\i_sdmilitia\Data\UI\Sudan.paa";
+		model="\A3\Characters_F\Common\Suitpacks\suitpack_universal_F.p3d";
+		class ItemInfo: UniformItem
+		{
+			uniformModel="-";
+			uniformClass="CFP_I_SDMILITA_Soldier_base_Sudan_01";
+			containerClass="Supply60";
+			mass=20;
+		};
+	};
+};
