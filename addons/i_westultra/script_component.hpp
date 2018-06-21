e38f772e341f30b90be02767b63a3a22a8b1a193
Tupolov <tupolov73@gmail.com>
2018-06-21 11:32:05 +0100

    [i_westultra] Update logistics and fix script_component

--- addons/i_westultra/script_component.hpp
+++ addons/i_westultra/script_component.hpp
@@ -1,13 +1,13 @@
-#define COMPONENT o_ultra
-#define COMPONENT_BEAUTIFIED o_ultra
+#define COMPONENT i_westultra
+#define COMPONENT_BEAUTIFIED i_westultra
 #include "\x\cfp\addons\main\script_mod.hpp"
 // #define DEBUG_MODE_FULL
 // #define DISABLE_COMPILE_CACHE
 // #define ENABLE_PERFORMANCE_COUNTERS
-#ifdef DEBUG_ENABLED_O_ULTRA
+#ifdef DEBUG_ENABLED_I_WESTULTRA
 #define DEBUG_MODE_FULL
 #endif
-#ifdef DEBUG_SETTINGS_O_ULTRA
-#define DEBUG_SETTINGS DEBUG_SETTINGS_O_ULTRA
+#ifdef DEBUG_SETTINGS_I_WESTULTRA
+#define DEBUG_SETTINGS DEBUG_SETTINGS_I_WESTULTRA
 #endif
 #include "\x\cfp\addons\main\script_macros.hpp"
