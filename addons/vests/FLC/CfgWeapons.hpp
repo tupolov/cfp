94cb7f0d316007f4d019ab160e0ff93f736ef3a3
AUTigerGrad <audrew77@yahoo.com>
2018-10-21 22:31:54 -0400

    Updated textures to ANA, ANP factions, added new vest variations

--- addons/vests/FLC/CfgWeapons.hpp
+++ addons/vests/FLC/CfgWeapons.hpp
@@ -152,3 +152,331 @@ class CfgVehicleClasses
 		};
 	};
 
+	class CFP_FAPC_Light_OGA: V_PlateCarrier1_rgr
+    {
+        picture = "\x\cfp\addons\vsm_vests\data\ui\icon_CFP_FAPC_Operator_OGA.paa";
+		scope = 2;
+		displayName = "OGA DT FAPC (Light)";
+		author = "CFP";
+		// picture = "\x\cfp\addons\vsm_vests\data\VSM_OGA.paa";
+		model = "x\cfp\addons\models\FAPC_Light.p3d";
+		descriptionShort = "$STR_A3_SP_AL_IV";
+		hiddenSelections[] = {"_vestBase"};
+		hiddenSelectionsTextures[] = {"x\cfp\addons\vsm_vests\data\VSM_fapc_OGA.paa"};
+		class ItemInfo: VestItem
+		{
+			uniformModel = "x\cfp\addons\models\FAPC_Light.p3d";
+			containerClass = "Supply200";
+			mass = 50;
+			passThrough = 1;
+			hiddenSelections[] = {"_vestBase"};
+			class HitpointsProtectionInfo
+			{
+				class Chest
+				{
+					HitpointName = "HitChest";
+					armor = 25;
+					PassThrough = 0.3;
+				};
+				class Diaphragm
+				{
+					HitpointName = "HitDiaphragm";
+					armor = 25;
+					PassThrough = 0.3;
+				};
+				class Abdomen
+				{
+					hitpointName = "HitAbdomen";
+					armor = 25;
+					passThrough = 0.3;
+				};
+				class Body
+				{
+					hitpointName = "HitBody";
+					passThrough = 0.3;
+				};
+			};
+		};
+	};
+
+	class CFP_LBT6094_Light_OGA: V_PlateCarrier1_rgr
+    {
+        picture = "\x\cfp\addons\vsm_vests\data\ui\icon_CFP_LBT6094_operator_OGA.paa";
+		scope = 2;
+		author = "CFP";
+		displayName = "OGA LBT6094 (Light)";
+		// picture = "\x\cfp\addons\vsm_vests\data\VSM_OGA.paa";
+		model = "\x\cfp\addons\models\LBT6094_Light.p3d";
+		hiddenSelections[] = {"_battleBelt","_SOTGGear","_SOTGRadio","_vestBase","_VSMPouches"};
+		hiddenSelectionsTextures[] = {"x\cfp\addons\vsm_vests\data\VSM_battlebelt_OGA.paa","x\cfp\addons\vsm_vests\data\VSM_OGA_gear_co.paa","x\cfp\addons\vsm_vests\data\radio_pouch.paa","x\cfp\addons\vsm_vests\data\VSM_lbt6094_OGA.paa","x\cfp\addons\vsm_vests\data\VSM_pouches_OGA.paa"};
+		descriptionShort = "$STR_A3_SP_AL_IV";
+		class ItemInfo: VestItem
+		{
+			uniformModel = "\x\cfp\addons\models\LBT6094_Light.p3d";
+			containerClass = "Supply200";
+			mass = 50;
+			passThrough = 1;
+			hiddenSelections[] = {"_battleBelt","_SOTGGear","_SOTGRadio","_vestBase","_VSMPouches"};
+			class HitpointsProtectionInfo
+			{
+				class Chest
+				{
+					HitpointName = "HitChest";
+					armor = 25;
+					PassThrough = 0.3;
+				};
+				class Diaphragm
+				{
+					HitpointName = "HitDiaphragm";
+					armor = 25;
+					PassThrough = 0.3;
+				};
+				class Abdomen
+				{
+					hitpointName = "HitAbdomen";
+					armor = 25;
+					passThrough = 0.3;
+				};
+				class Body
+				{
+					hitpointName = "HitBody";
+					passThrough = 0.3;
+				};
+			};
+		};
+	};
+
+	class CFP_LBT6094_Empty_OGA: V_PlateCarrier1_rgr
+    {
+        picture = "\x\cfp\addons\vsm_vests\data\ui\icon_CFP_LBT6094_operator_OGA.paa";
+		scope = 2;
+		author = "CFP";
+		displayName = "OGA LBT6094 (Empty)";
+		// picture = "\x\cfp\addons\vsm_vests\data\VSM_OGA.paa";
+		model = "\x\cfp\addons\models\LBT6094_Empty.p3d";
+		hiddenSelections[] = {"_vestBase"};
+		hiddenSelectionsTextures[] = {"x\cfp\addons\vsm_vests\data\VSM_lbt6094_OGA.paa"};
+		descriptionShort = "$STR_A3_SP_AL_IV";
+		class ItemInfo: VestItem
+		{
+			uniformModel = "\x\cfp\addons\models\LBT6094_Empty.p3d";
+			containerClass = "Supply200";
+			mass = 50;
+			passThrough = 1;
+			hiddenSelections[] = {"_vestBase"};
+			class HitpointsProtectionInfo
+			{
+				class Chest
+				{
+					HitpointName = "HitChest";
+					armor = 25;
+					PassThrough = 0.3;
+				};
+				class Diaphragm
+				{
+					HitpointName = "HitDiaphragm";
+					armor = 25;
+					PassThrough = 0.3;
+				};
+				class Abdomen
+				{
+					hitpointName = "HitAbdomen";
+					armor = 25;
+					passThrough = 0.3;
+				};
+				class Body
+				{
+					hitpointName = "HitBody";
+					passThrough = 0.3;
+				};
+			};
+		};
+	};
+
+	class CFP_RAV_Green: V_PlateCarrier1_rgr
+    {
+        picture = "\x\cfp\addons\vsm_vests\data\ui\icon_CFP_LBT6094_operator_OGA.paa";
+		scope = 2;
+		author = "CFP";
+		displayName = "RAV Vest (RGR Green)";
+		// picture = "\x\cfp\addons\vsm_vests\data\VSM_OGA.paa";
+		model = "\x\cfp\addons\models\CFP_RAV.p3d";
+		hiddenSelections[] = {"camo"};
+		hiddenSelectionsTextures[] = {"x\cfp\addons\vsm_vests\data\spec_rav_co.paa"};
+		descriptionShort = "$STR_A3_SP_AL_IV";
+		class ItemInfo: VestItem
+		{
+			uniformModel = "\x\cfp\addons\models\CFP_RAV.p3d";
+			containerClass = "Supply200";
+			mass = 50;
+			passThrough = 1;
+			hiddenSelections[] = {"camo"};
+			class HitpointsProtectionInfo
+			{
+				class Chest
+				{
+					HitpointName = "HitChest";
+					armor = 25;
+					PassThrough = 0.3;
+				};
+				class Diaphragm
+				{
+					HitpointName = "HitDiaphragm";
+					armor = 25;
+					PassThrough = 0.3;
+				};
+				class Abdomen
+				{
+					hitpointName = "HitAbdomen";
+					armor = 25;
+					passThrough = 0.3;
+				};
+				class Body
+				{
+					hitpointName = "HitBody";
+					passThrough = 0.3;
+				};
+			};
+		};
+	};
+
+	class CFP_RAV_Empty_Green: V_PlateCarrier1_rgr
+    {
+        picture = "\x\cfp\addons\vsm_vests\data\ui\icon_CFP_LBT6094_operator_OGA.paa";
+		scope = 2;
+		author = "CFP";
+		displayName = "RAV Vest Empty (RGR Green)";
+		// picture = "\x\cfp\addons\vsm_vests\data\VSM_OGA.paa";
+		model = "\x\cfp\addons\models\RAV_Empty.p3d";
+		hiddenSelections[] = {"camo"};
+		hiddenSelectionsTextures[] = {"x\cfp\addons\vsm_vests\data\spec_rav_co.paa"};
+		descriptionShort = "$STR_A3_SP_AL_IV";
+		class ItemInfo: VestItem
+		{
+			uniformModel = "\x\cfp\addons\models\RAV_Empty.p3d";
+			containerClass = "Supply200";
+			mass = 50;
+			passThrough = 1;
+			hiddenSelections[] = {"camo"};
+			class HitpointsProtectionInfo
+			{
+				class Chest
+				{
+					HitpointName = "HitChest";
+					armor = 25;
+					PassThrough = 0.3;
+				};
+				class Diaphragm
+				{
+					HitpointName = "HitDiaphragm";
+					armor = 25;
+					PassThrough = 0.3;
+				};
+				class Abdomen
+				{
+					hitpointName = "HitAbdomen";
+					armor = 25;
+					passThrough = 0.3;
+				};
+				class Body
+				{
+					hitpointName = "HitBody";
+					passThrough = 0.3;
+				};
+			};
+		};
+	};
+
+	class CFP_RAV_Black: V_PlateCarrier1_rgr
+    {
+        picture = "\x\cfp\addons\vsm_vests\data\ui\icon_CFP_LBT6094_operator_OGA.paa";
+		scope = 2;
+		author = "CFP";
+		displayName = "RAV Vest (Black)";
+		// picture = "\x\cfp\addons\vsm_vests\data\VSM_OGA.paa";
+		model = "\x\cfp\addons\models\CFP_RAV.p3d";
+		hiddenSelections[] = {"camo"};
+		hiddenSelectionsTextures[] = {"x\cfp\addons\vsm_vests\data\spec_rav_blk_co.paa"};
+		descriptionShort = "$STR_A3_SP_AL_IV";
+		class ItemInfo: VestItem
+		{
+			uniformModel = "\x\cfp\addons\models\CFP_RAV.p3d";
+			containerClass = "Supply200";
+			mass = 50;
+			passThrough = 1;
+			hiddenSelections[] = {"camo"};
+			class HitpointsProtectionInfo
+			{
+				class Chest
+				{
+					HitpointName = "HitChest";
+					armor = 25;
+					PassThrough = 0.3;
+				};
+				class Diaphragm
+				{
+					HitpointName = "HitDiaphragm";
+					armor = 25;
+					PassThrough = 0.3;
+				};
+				class Abdomen
+				{
+					hitpointName = "HitAbdomen";
+					armor = 25;
+					passThrough = 0.3;
+				};
+				class Body
+				{
+					hitpointName = "HitBody";
+					passThrough = 0.3;
+				};
+			};
+		};
+	};
+
+	class CFP_RAV_OGA: V_PlateCarrier1_rgr
+    {
+        picture = "\x\cfp\addons\vsm_vests\data\ui\icon_CFP_LBT6094_operator_OGA.paa";
+		scope = 2;
+		author = "CFP";
+		displayName = "RAV Vest (OGA)";
+		// picture = "\x\cfp\addons\vsm_vests\data\VSM_OGA.paa";
+		model = "\x\cfp\addons\models\CFP_RAV.p3d";
+		hiddenSelections[] = {"camo"};
+		hiddenSelectionsTextures[] = {"x\cfp\addons\vsm_vests\data\vsm_rav_OGA.paa"};
+		descriptionShort = "$STR_A3_SP_AL_IV";
+		class ItemInfo: VestItem
+		{
+			uniformModel = "\x\cfp\addons\models\CFP_RAV.p3d";
+			containerClass = "Supply200";
+			mass = 50;
+			passThrough = 1;
+			hiddenSelections[] = {"camo"};
+			class HitpointsProtectionInfo
+			{
+				class Chest
+				{
+					HitpointName = "HitChest";
+					armor = 25;
+					PassThrough = 0.3;
+				};
+				class Diaphragm
+				{
+					HitpointName = "HitDiaphragm";
+					armor = 25;
+					PassThrough = 0.3;
+				};
+				class Abdomen
+				{
+					hitpointName = "HitAbdomen";
+					armor = 25;
+					passThrough = 0.3;
+				};
+				class Body
+				{
+					hitpointName = "HitBody";
+					passThrough = 0.3;
+				};
+			};
+		};
+	};
--- addons/vests/OspreyMk2/CfgWeapons.hpp
+++ addons/vests/OspreyMk2/CfgWeapons.hpp
@@ -4,10 +4,18 @@
 {
     picture = "\x\cfp\addons\vests\data\ui\icon_CFP_Osprey1_Wd.paa";
 		scope = 2;
-		displayName = "Osprey Mk2 (Woodland - Empty)";
+		displayName = "Osprey Mk2 (M81 - Empty)";
 		// picture = "\x\cfp\addons\vests\OspreyMk2\ui\icon_v_mk2_empty_wd.paa";
 		hiddenSelectionsTextures[] = {"\x\cfp\addons\vests\OspreyMk2\mk2_empty_m81.paa","\x\cfp\addons\vests\OspreyMk2\mk2_empty_m81.paa","\x\cfp\addons\vests\OspreyMk2\mk2_empty_m81.paa","\x\cfp\addons\vests\OspreyMk2\mk2_empty_m81.paa"};
 	};
 
+class CFP_Osprey1_Green: CUP_V_B_DDPM_Osprey_Mk3_Empty
+{
+    picture = "\x\cfp\addons\vests\data\ui\icon_CFP_Osprey1_Wd.paa";
+		scope = 2;
+		displayName = "Osprey Mk2 (Green - Empty)";
+		// picture = "\x\cfp\addons\vests\OspreyMk2\ui\icon_v_mk2_empty_wd.paa";
+		hiddenSelectionsTextures[] = {"\x\cfp\addons\vests\OspreyMk2\mk2_empty_green.paa","\x\cfp\addons\vests\OspreyMk2\mk2_empty_green.paa","\x\cfp\addons\vests\OspreyMk2\mk2_empty_green.paa","\x\cfp\addons\vests\OspreyMk2\mk2_empty_green.paa"};
+	};
 
 
