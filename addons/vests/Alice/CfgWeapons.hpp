93fdb351f681c6ab2aac6197fe8f15cc31002727
AUTigerGrad <audrew77@yahoo.com>
2018-11-18 13:25:10 -0500

    Added Black and Tan Alice Webbing variants, both Field Uniform and Battle Dress Uniform variants

--- addons/vests/Alice/CfgWeapons.hpp
+++ addons/vests/Alice/CfgWeapons.hpp
@@ -57,11 +57,103 @@ class CfgVehicleClasses
 		};
 	};
 
+	class CFP_Alice_Vest_Tan: V_PlateCarrier1_rgr
+	{
+	    picture = "\x\cfp\addons\vests\data\ui\icon_CFP_Smersh_Brown.paa";
+		scope = 2;
+		displayName = "Alice Vest (Tan)";
+		// picture = "\CUP\Creatures\People\Military\CUP_Creatures_People_Military_Russia\data\ui\icon_v_smerch_1_ca.paa";
+		model = "\x\cfp\addons\vests\Alice\alice_sample.p3d";
+		hiddenSelectionsTextures[] = {"\x\cfp\addons\vests\Alice\tex\equip_nato_brown.paa","\x\cfp\addons\vests\Alice\tex\wg_co_dcu.paa"};
+		hiddenSelections[] = {"Camo1","Camo2"};
+		author = "AuburnAlumni";
+		class ItemInfo: VestItem
+		{
+			uniformModel = "\x\cfp\addons\vests\Alice\alice_sample.p3d";
+			containerClass = "Supply140";
+			hiddenSelections[] = {"Camo1","Camo2"};
+			mass = 100;
+			class HitpointsProtectionInfo
+			{
+				class Chest
+				{
+					hitpointName = "HitChest";
+					armor = 16;
+					passThrough = 0.3;
+				};
+				class Diaphragm
+				{
+					hitpointName = "HitDiaphragm";
+					armor = 16;
+					passThrough = 0.3;
+				};
+				class Abdomen
+				{
+					hitpointName = "HitAbdomen";
+					armor = 16;
+					passThrough = 0.3;
+				};
+				class Body
+				{
+					hitpointName = "HitBody";
+					passThrough = 0.3;
+				};
+			};
+
+		};
+	};
+
+	class CFP_Alice_Vest_Black: V_PlateCarrier1_rgr
+	{
+	    picture = "\x\cfp\addons\vests\data\ui\icon_CFP_Smersh_Brown.paa";
+		scope = 2;
+		displayName = "Alice Vest (Black)";
+		// picture = "\CUP\Creatures\People\Military\CUP_Creatures_People_Military_Russia\data\ui\icon_v_smerch_1_ca.paa";
+		model = "\x\cfp\addons\vests\Alice\alice_sample.p3d";
+		hiddenSelectionsTextures[] = {"\x\cfp\addons\vests\Alice\tex\equip_nato_black.paa","\x\cfp\addons\vests\Alice\tex\wg_co_black.paa"};
+		hiddenSelections[] = {"Camo1","Camo2"};
+		author = "AuburnAlumni";
+		class ItemInfo: VestItem
+		{
+			uniformModel = "\x\cfp\addons\vests\Alice\alice_sample.p3d";
+			containerClass = "Supply140";
+			hiddenSelections[] = {"Camo1","Camo2"};
+			mass = 100;
+			class HitpointsProtectionInfo
+			{
+				class Chest
+				{
+					hitpointName = "HitChest";
+					armor = 16;
+					passThrough = 0.3;
+				};
+				class Diaphragm
+				{
+					hitpointName = "HitDiaphragm";
+					armor = 16;
+					passThrough = 0.3;
+				};
+				class Abdomen
+				{
+					hitpointName = "HitAbdomen";
+					armor = 16;
+					passThrough = 0.3;
+				};
+				class Body
+				{
+					hitpointName = "HitBody";
+					passThrough = 0.3;
+				};
+			};
+
+		};
+	};
+
 	class CFP_Alice_Vest2: V_PlateCarrier1_rgr
 	{
 	    picture = "\x\cfp\addons\vests\data\ui\icon_CFP_Smersh_Brown.paa";
 		scope = 2;
-		displayName = "Alice Vest (Version 2)";
+		displayName = "Alice Vest (BDU Version)";
 		// picture = "\CUP\Creatures\People\Military\CUP_Creatures_People_Military_Russia\data\ui\icon_v_smerch_1_ca.paa";
 		model = "\x\cfp\addons\vests\Alice\alice_sample2.p3d";
 		hiddenSelectionsTextures[] = {""};
@@ -72,6 +164,98 @@ class CfgVehicleClasses
 			uniformModel = "\x\cfp\addons\vests\Alice\alice_sample2.p3d";
 			containerClass = "Supply140";
 			hiddenSelections[] = {""};
+			mass = 100;
+			class HitpointsProtectionInfo
+			{
+				class Chest
+				{
+					hitpointName = "HitChest";
+					armor = 16;
+					passThrough = 0.3;
+				};
+				class Diaphragm
+				{
+					hitpointName = "HitDiaphragm";
+					armor = 16;
+					passThrough = 0.3;
+				};
+				class Abdomen
+				{
+					hitpointName = "HitAbdomen";
+					armor = 16;
+					passThrough = 0.3;
+				};
+				class Body
+				{
+					hitpointName = "HitBody";
+					passThrough = 0.3;
+				};
+			};
+
+		};
+	};
+
+	class CFP_Alice_Vest2_Tan: V_PlateCarrier1_rgr
+	{
+	    picture = "\x\cfp\addons\vests\data\ui\icon_CFP_Smersh_Brown.paa";
+		scope = 2;
+		displayName = "Alice Vest (BDU Version Tan)";
+		// picture = "\CUP\Creatures\People\Military\CUP_Creatures_People_Military_Russia\data\ui\icon_v_smerch_1_ca.paa";
+		model = "\x\cfp\addons\vests\Alice\alice_sample2.p3d";
+		hiddenSelectionsTextures[] = {"\x\cfp\addons\vests\Alice\tex\equip_nato_brown.paa","\x\cfp\addons\vests\Alice\tex\wg_co_dcu.paa"};
+		hiddenSelections[] = {"Camo1","Camo2"};
+		author = "AuburnAlumni";
+		class ItemInfo: VestItem
+		{
+			uniformModel = "\x\cfp\addons\vests\Alice\alice_sample2.p3d";
+			containerClass = "Supply140";
+			hiddenSelections[] = {"Camo1","Camo2"};
+			mass = 100;
+			class HitpointsProtectionInfo
+			{
+				class Chest
+				{
+					hitpointName = "HitChest";
+					armor = 16;
+					passThrough = 0.3;
+				};
+				class Diaphragm
+				{
+					hitpointName = "HitDiaphragm";
+					armor = 16;
+					passThrough = 0.3;
+				};
+				class Abdomen
+				{
+					hitpointName = "HitAbdomen";
+					armor = 16;
+					passThrough = 0.3;
+				};
+				class Body
+				{
+					hitpointName = "HitBody";
+					passThrough = 0.3;
+				};
+			};
+
+		};
+	};
+
+	class CFP_Alice_Vest2_Black: V_PlateCarrier1_rgr
+	{
+	    picture = "\x\cfp\addons\vests\data\ui\icon_CFP_Smersh_Brown.paa";
+		scope = 2;
+		displayName = "Alice Vest (BDU Version Black)";
+		// picture = "\CUP\Creatures\People\Military\CUP_Creatures_People_Military_Russia\data\ui\icon_v_smerch_1_ca.paa";
+		model = "\x\cfp\addons\vests\Alice\alice_sample2.p3d";
+		hiddenSelectionsTextures[] = {"\x\cfp\addons\vests\Alice\tex\equip_nato_black.paa","\x\cfp\addons\vests\Alice\tex\wg_co_black.paa"};
+		hiddenSelections[] = {"Camo1","Camo2"};
+		author = "AuburnAlumni";
+		class ItemInfo: VestItem
+		{
+			uniformModel = "\x\cfp\addons\vests\Alice\alice_sample2.p3d";
+			containerClass = "Supply140";
+			hiddenSelections[] = {"Camo1","Camo2"};
 			mass = 100;
 			class HitpointsProtectionInfo
 			{
