54600381569818d6da7e048933520db54286b8f0
HerrOPS <49thops@gmail.com>
2018-12-21 21:11:16 -0700

    Gunfighter Stache Added

--- addons/headgear/HeloHelmet/CfgWeapons.hpp
+++ addons/headgear/HeloHelmet/CfgWeapons.hpp
@@ -90,3 +90,31 @@ class CFP_SOARCREW_BLUE: ItemCore
 
 	};
 };
+
+class CFP_SOARCREW_STACHE: ItemCore
+    {
+    picture = "\A3\Characters_F\data\ui\icon_H_Crew_Helmet_Heli_B_CA.paa";
+	scope = 2;
+	weaponPoolAvailable = 1;
+	displayName="SOAR Crew Helmet (Gunfighter Stache)";
+	model="A3\Characters_F\Common\headgear_helmet_heli_shield";
+	hiddenSelectionsTextures[] = {"x\cfp\addons\headgear\HeloHelmet\stache.paa"};
+	hiddenSelections[] = {"Camo"};
+	author = "CFP";
+        ace_hearing_protection = 0.85;  // Protection against deafening (0 to 1, higher means more protection)
+        ace_hearing_lowerVolume = 0.60;  // Muffling of the sound (0 to 1, higher means more muffling)
+
+	class ItemInfo: HeadgearItem
+	{
+		mass = 60;
+		uniformmodel = "A3\Characters_F\Common\headgear_helmet_heli_shield";
+		modelSides[] = {3,1};
+		armor = 6;
+		passThrough = 0.50;
+		HITPOINTS_HEAD(10,0.30)
+		hiddenSelectionsTextures[] = {"x\cfp\addons\headgear\HeloHelmet\stache.paa"};
+		hiddenSelections[] = {"Camo"};
+
+	};
+};
+
