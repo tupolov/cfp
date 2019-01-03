3934b1fa3e1f9dfcac22ee03a1ec25216ad3abe6
Tupolov <tupolov73@gmail.com>
2019-01-03 13:07:55 +0000

    [cup_b_us_army] Update gunners to inherit from correct class

--- addons/cup_b_us_army/CfgVehicles.hpp
+++ addons/cup_b_us_army/CfgVehicles.hpp
@@ -108,45 +108,46 @@ class CfgVehicles {
             displayName = "Forward Air Controller";
         };
 
-        class CUP_B_US_ARMY_Gunner_M2_High_DES_01 : CUP_B_USArmy_Soldier_01 {
+        class CUP_B_US_Soldier;
+        class CUP_B_US_ARMY_Gunner_M2_High_DES_01 : CUP_B_US_Soldier {
             displayName = "Gunner [MG High]";
             backpack = "CUP_B_M2_Gun_Bag";
         };
 
-        class CUP_B_US_ARMY_Gunner_M2_Low_DES_01 : CUP_B_USArmy_Soldier_01 {
+        class CUP_B_US_ARMY_Gunner_M2_Low_DES_01 : CUP_B_US_Soldier {
             displayName = "Gunner [MG Low]";
             backpack = "CUP_B_M2_Gun_Bag";
 
         };
 
-        class CUP_B_US_ARMY_Asst_Gunner_M2_DES_01 : CUP_B_USArmy_Soldier_01 {
+        class CUP_B_US_ARMY_Asst_Gunner_M2_DES_01 : CUP_B_US_Soldier {
             displayName = "Asst. Gunner [MG]";
             backpack = "CUP_B_M2_Tripod_Bag";
         };
 
-        class CUP_B_US_ARMY_Asst_Gunner_M2Low_DES_01 : CUP_B_USArmy_Soldier_01 {
+        class CUP_B_US_ARMY_Asst_Gunner_M2Low_DES_01 : CUP_B_US_Soldier {
             displayName = "Asst. Gunner [MG Low]";
             backpack = "CUP_B_M2_Minitripod_Bag";
         };
 
-        class CUP_B_US_ARMY_Gunner_TOW_DES_01 : CUP_B_USArmy_Soldier_01 {
+        class CUP_B_US_ARMY_Gunner_TOW_DES_01 : CUP_B_US_Soldier {
             displayName = "Gunner [AT]";
             backpack = "CUP_B_Tow_Gun_Bag";
         };
 
-        class CUP_B_US_ARMY_Asst_Gunner_TOW_DES_01 : CUP_B_USArmy_Soldier_01 {
+        class CUP_B_US_ARMY_Asst_Gunner_TOW_DES_01 : CUP_B_US_Soldier {
             displayName = "Asst. Gunner [AT]";
             backpack = "CUP_B_TOW_Tripod_Bag";
 
         };
 
-        class CUP_B_US_ARMY_Gunner_Mortar_DES_01 : CUP_B_USArmy_Soldier_01 {
+        class CUP_B_US_ARMY_Gunner_Mortar_DES_01 : CUP_B_US_Soldier {
 
             displayName = "Gunner [Mortar]";
             backpack = "CUP_B_M252_Gun_Bag";
         };
 
-        class CUP_B_US_ARMY_Asst_Gunner_Mortar_DES_01 : CUP_B_USArmy_Soldier_01 {
+        class CUP_B_US_ARMY_Asst_Gunner_Mortar_DES_01 : CUP_B_US_Soldier {
             displayName = "Asst. Gunner [Mortar]";
             backpack = "CUP_B_M252_Bipod_Bag";
         };
