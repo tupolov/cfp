ebd5d2b1ba31dc89c4d51483bb647c1825decf5f
AUTigerGrad <audrew77@yahoo.com>
2019-08-11 16:54:09 -0400

    Added 1991 Desert Storm Era US Army Faction (Arid/Desert)

---
+++ addons/b_usarmy_1991_desert/script_component.hpp
@@ -0,0 +1,13 @@
+#define COMPONENT cfp_b_usarmy_1991_des
+#define COMPONENT_BEAUTIFIED cfp_b_usarmy_1991_des
+#include "\x\cfp\addons\main\script_mod.hpp"
+// #define DEBUG_MODE_FULL
+// #define DISABLE_COMPILE_CACHE
+// #define ENABLE_PERFORMANCE_COUNTERS
+#ifdef DEBUG_ENABLED_CFP_B_USARMY_1991_DES
+#define DEBUG_MODE_FULL
+#endif
+#ifdef DEBUG_SETTINGS_CFP_B_USARMY_1991_DES
+#define DEBUG_SETTINGS DEBUG_SETTINGS_CFP_B_USARMY_1991_DES
+#endif
+#include "\x\cfp\addons\main\script_macros.hpp"
