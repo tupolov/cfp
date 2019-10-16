3aa5e58c5d08aef37baaa1a62833435d2e86f105
Tupolov <tupolov73@gmail.com>
2019-10-16 12:46:38 +0100

    [b_usarmy] Rename new US Army historic factions to use naming standards

---
+++ addons/b_usarmy_2003_wdl/script_component.hpp
@@ -0,0 +1,13 @@
+#define COMPONENT cfp_b_usarmy_2003_wdl
+#define COMPONENT_BEAUTIFIED cfp_b_usarmy_2003_wdl
+#include "\x\cfp\addons\main\script_mod.hpp"
+// #define DEBUG_MODE_FULL
+// #define DISABLE_COMPILE_CACHE
+// #define ENABLE_PERFORMANCE_COUNTERS
+#ifdef DEBUG_ENABLED_CFP_B_USARMY_2003_WDL
+#define DEBUG_MODE_FULL
+#endif
+#ifdef DEBUG_SETTINGS_CFP_B_USARMY_2003_WDL
+#define DEBUG_SETTINGS DEBUG_SETTINGS_CFP_B_USARMY_2003_WDL
+#endif
+#include "\x\cfp\addons\main\script_macros.hpp"
