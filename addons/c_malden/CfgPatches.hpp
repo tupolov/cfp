2a2ac035722a890ce48333e5502d10b711296770
AUTigerGrad <audrew77@yahoo.com>
2020-03-15 20:17:17 -0400

    Added Malden Civilian Faction including Custom French Names

---
+++ addons/c_malden/CfgPatches.hpp
@@ -0,0 +1,53 @@
+class CfgPatches {
+    class CFP_C_MALDEN {
+        units[] = {
+            "CFP_C_MALDEN_Civilian_01",
+            "CFP_C_MALDEN_Civilian_02",
+            "CFP_C_MALDEN_Offroad_01",
+            "CFP_C_MALDEN_Hatchback_01",
+            "CFP_C_MALDEN_SUV_01",
+            "CFP_C_MALDEN_Truck_Boxer_01",
+            "CFP_C_MALDEN_Truck_01",
+            "CFP_C_MALDEN_MB_4WD_01",
+            "CFP_C_MALDEN_Golf_Random_Color_01",
+            "CFP_C_MALDEN_Offroad_Covered_01",
+            "CFP_C_MALDEN_Van_Transport_01",
+            "CFP_C_MALDEN_Hilux_01"
+        };
+        weapons[] = {};
+        requiredVersion = 1.62;
+        requiredAddons[] = {
+            "A3_Characters_F",
+            "A3_Characters_F_Exp_Civil",
+            "cfp_uniforms",
+            "A3_Sounds_F",
+            "A3_Sounds_F_Exp",
+             "A3_Data_F",
+            "A3_Data_F_Curator",
+            "A3_Air_F_Heli",
+            "CUP_Worlds_DustEffects",
+            "CUP_Core",
+            "cba_xeh",
+            "A3_Soft_F_Offroad_01",
+            "A3_Soft_F_Exp_Offroad_01",
+            "A3_Soft_F",
+            "A3_Soft_F_Gamma_Hatchback_01",
+            "A3_Soft_F_Exp_Offroad_02",
+            "A3_Data_F_ParticleEffects",
+            "A3_Armor_F_Beta",
+            "A3_Soft_F_Gamma_SUV_01",
+            "A3_Soft_F_Gamma_Van_01",
+            "A3_Soft_F_Beta_Truck_01",
+            "A3_Soft_F_Beta_Truck_02",
+            "A3_Soft_F_Gamma_Truck_01",
+            "A3_Soft_F_EPC_Truck_03",
+            "A3_Soft_F_Orange_Van_02",
+            "A3_Soft_F_Enoch_Offroad_01",
+            "A3_Sounds_F_Orange",
+            "CUP_WheeledVehicles_Hilux"
+        };
+        author = "Drew";
+        authors[] = {"Drew"};
+    };
+};
+
