ef78ac6c618465bda17ed8967a49608b2759cf02
AUTigerGrad <audrew77@yahoo.com>
2020-03-15 20:16:39 -0400

    Added Cherno Winter Civilian Faction (Initial Version)

---
+++ addons/c_cherno_winter/script_component.hpp
@@ -0,0 +1,13 @@
+#define COMPONENT c_cherno_win
+#define COMPONENT_BEAUTIFIED c_cherno_win
+#include "\x\cfp\addons\main\script_mod.hpp"
+// #define DEBUG_MODE_FULL
+// #define DISABLE_COMPILE_CACHE
+// #define ENABLE_PERFORMANCE_COUNTERS
+#ifdef DEBUG_ENABLED_C_CHERNO_WIN
+#define DEBUG_MODE_FULL
+#endif
+#ifdef DEBUG_SETTINGS_C_CHERNO_WIN
+#define DEBUG_SETTINGS DEBUG_SETTINGS_C_CHERNO_WIN
+#endif
+#include "\x\cfp\addons\main\script_macros.hpp"
