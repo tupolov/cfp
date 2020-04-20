a2a531d4d8ab3c8fc9c7b57474d2428ec7799239
AUTigerGrad <audrew77@yahoo.com>
2020-04-20 08:31:13 -0400

    Improved Textures of M23 Pioneer AK Vests, added Full variants

--- addons/vests/AK_Vest/CfgWeapons.hpp
+++ addons/vests/AK_Vest/CfgWeapons.hpp
@@ -9,7 +9,7 @@ class CFP_AK_VEST: Vest_Camo_Base
 	author = "CFP";
 	model = "x\cfp\addons\vests\AK_Vest\data\model\UO_AK_VEST.p3d";
 	hiddenSelections[] = {"camo"};
-	hiddenSelectionsTextures[] = {"x\cfp\addons\vests\AK_Vest\data\tak_soldier_equip_liz_co.paa"};
+	hiddenSelectionsTextures[] = {"x\cfp\addons\vests\AK_Vest\data\tak_soldier_equip_co.paa"};
 
 
 	class ItemInfo: VestItem
@@ -47,7 +47,7 @@ class CFP_AK_VEST_LOlive: CFP_AK_VEST
 {
     picture = "\x\cfp\addons\vests\data\ui\icon_CFP_AK_VEST_LOlive.paa";
 
-	displayName = "AK Chest Rig Light Olive";
+	displayName = "AK Chest Rig OD";
 	hiddenSelectionsTextures[] = {"x\cfp\addons\vests\AK_Vest\data\tak_soldier_equip_lightolive_co.paa"};
 };
 
@@ -58,6 +58,80 @@ class CFP_AK_VEST_Olive: CFP_AK_VEST
 	displayName = "AK Chest Rig Olive";
 	hiddenSelectionsTextures[] = {"x\cfp\addons\vests\AK_Vest\data\tak_soldier_equip_olive_co.paa"};
 
+};
+
+class CFP_AK_VEST_EMR: CFP_AK_VEST
+{
+    picture = "\x\cfp\addons\vests\data\ui\icon_CFP_AK_VEST_Olive.paa";
+
+	displayName = "AK Chest Rig EMR";
+	hiddenSelectionsTextures[] = {"x\cfp\addons\vests\AK_Vest\data\tak_soldier_equip_emr_co.paa"};
+};
+
+class CFP_AK_VEST_VSR98: CFP_AK_VEST
+{
+    picture = "\x\cfp\addons\vests\data\ui\icon_CFP_AK_VEST_Olive.paa";
+
+	displayName = "AK Chest Rig VSR-98";
+	hiddenSelectionsTextures[] = {"x\cfp\addons\vests\AK_Vest\data\tak_soldier_equip_vsr98_co.paa"};
+};
+
+class CFP_M23_VEST_VSR98: Vest_Camo_Base
+{
+    picture = "\x\cfp\addons\vests\data\ui\icon_CFP_AK_VEST.paa";
+	scope = 2;
+	displayName = "M23 Pioneer (VSR-98)";
+	// picture = "\x\cfp\addons\vests\AK_Vest\data\icon_chest_rig.paa";
+	author = "CFP";
+	model = "x\cfp\addons\vests\AK_Vest\data\model\CFP_TK_Vest1.p3d";
+	hiddenSelections[] = {"camo1"};
+	hiddenSelectionsTextures[] = {"x\cfp\addons\vests\AK_Vest\data\tak_soldier_equip_vsr98_co.paa"};
+
+
+	class ItemInfo: VestItem
+	{
+
+		uniformModel = "x\cfp\addons\vests\AK_Vest\data\model\CFP_TK_Vest1.p3d";
+		containerClass = "Supply100";
+		mass = 30;
+		armor = "2";
+		passThrough = true;
+		hiddenSelections[] = {"camo1"};
+		hiddenSelectionsTextures[] = {"x\cfp\addons\vests\AK_Vest\data\tak_soldier_equip_vsr98_co.paa"};
+
+	};
+};
+
+class CFP_M23_VEST_EMR: CFP_M23_VEST_VSR98
+{
+    picture = "\x\cfp\addons\vests\data\ui\icon_CFP_AK_VEST_Olive.paa";
+
+	displayName = "M23 Pioneer (EMR)";
+	hiddenSelectionsTextures[] = {"x\cfp\addons\vests\AK_Vest\data\tak_soldier_equip_emr_co.paa"};
+};
+
+class CFP_M23_VEST_MCam: CFP_M23_VEST_VSR98
+{
+    picture = "\x\cfp\addons\vests\data\ui\icon_CFP_AK_VEST_Olive.paa";
+
+	displayName = "M23 Pioneer (Multicam)";
+	hiddenSelectionsTextures[] = {"x\cfp\addons\vests\AK_Vest\data\tak_soldier_equip_mcam_co.paa"};
+};
+
+class CFP_M23_VEST_Lizard: CFP_M23_VEST_VSR98
+{
+    picture = "\x\cfp\addons\vests\data\ui\icon_CFP_AK_VEST_Olive.paa";
+
+	displayName = "M23 Pioneer (Taki Lizard)";
+	hiddenSelectionsTextures[] = {"x\cfp\addons\vests\AK_Vest\data\tak_soldier_equip_co.paa"};
+};
+
+class CFP_M23_VEST_Lime: CFP_M23_VEST_VSR98
+{
+    picture = "\x\cfp\addons\vests\data\ui\icon_CFP_AK_VEST_Olive.paa";
+
+	displayName = "M23 Pioneer (Lime Green)";
+	hiddenSelectionsTextures[] = {"x\cfp\addons\vests\AK_Vest\data\tak_soldier_equip_limegreen.paa"};
 };
 
 class CFP_AK_VEST_Tan: CFP_AK_VEST
